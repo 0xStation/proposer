98db5675afd91a5635f10236a3d8675378678e57
Michael Gingras <mcg79@cornell.edu>
2021-12-15 23:01:42 -0500

    feat: adding API endpoint for creating the SVG of the ticket

---
+++ app/api/nft.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { genSVG } from "../ticket/svg"
+
+type Data = {
+  encoded: string
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  let props = {
+    gradientColor: req.body.gradientColor,
+    name: req.body.name,
+    role: req.body.role,
+    terminal: req.body.terminal,
+  }
+
+  let nft = genSVG(props)
+  let encoded = nft ? Buffer.from(nft).toString("base64") : ""
+  res.status(200).json({ encoded })
+}
