9ae368cdb5e380230016940b2d5573fc2915231d
Michael Gingras <mcg79@cornell.edu>
2021-12-18 00:28:22 -0500

    component: navbar v1

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import { Image } from "blitz"
 import Ticker from "../components/Ticker"
 import Dropdown from "../components/Dropdown"
 import logo from "../../../public/station_logo.svg"
+import SoundIcon from "../icons/soundIcon"
 
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   return (
@@ -11,18 +12,11 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
         <title>{title || "station-web"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-12 w-full px-4 bg-tunnel-black flex items-center border-b border-b-[#646464]">
+      {/* what will eventually become the nav */}
+      <div className="h-12 w-full px-4 bg-tunnel-black flex flex-row justify-between border-b border-b-concrete">
         <Dropdown
-          button={
-            <Image
-              // trust me I hate this too but I have to do it for next.js to work.
-              // it does some annoying image optimization stuff that end up resizing the container.
-              // see here: https://github.com/vercel/next.js/issues/18915
-              className="h-[28px]"
-              src={logo}
-              alt="Station logo, the letters station spelled out."
-            />
-          }
+          className="self-center"
+          button={<Image src={logo} alt="Station logo, the letters station spelled out." />}
           items={[
             { name: "newstand", href: "/newstand" },
             { name: "contact the staff", href: "/contact" },
@@ -30,6 +24,18 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
             { name: "twitter", href: "/twitter" },
           ]}
         />
+
+        <div className="flex items-center">
+          <span className="p-4 border-l border-l-concrete uppercase text-marble-white text-lg">
+            Map
+          </span>
+          <span className="p-4 border-l border-l-concrete block">
+            <SoundIcon />
+          </span>
+          <span className="p-4 pr-0 uppercase text-magic-mint text-lg border-l border-l-concrete">
+            Enter Station
+          </span>
+        </div>
       </div>
       <Ticker />
       {children}
