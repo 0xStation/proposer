a27a931cf2bfc040b6f36fc479ffe2cecc2cb3e6
Michael Gingras <mcg79@cornell.edu>
2021-12-18 23:47:26 -0500

    component: navigation

--- app/core/components/Dropdown.tsx
+++ app/core/components/Dropdown.tsx
@@ -1,10 +1,12 @@
 import { Menu, Transition } from "@headlessui/react"
 import { Fragment } from "react"
+import Chevron from "../icons/Chevron"
 
 interface DropdownProps {
   button: string | JSX.Element
   items: Link[]
-  className: string
+  side: "right" | "left"
+  className?: string
 }
 interface Link {
   name: string
@@ -14,39 +16,46 @@ interface Link {
 /**
  * Run of the mill dropdown.
  */
-const Dropdown = ({ button, items, className }: DropdownProps) => {
+const Dropdown = ({ button, items, side, className }: DropdownProps) => {
   return (
     <Menu as="div" className={`relative ${className}`}>
-      {/* trust me I hate this too but I have to do it for next.js to work.
-       it does some annoying image optimization stuff that end up resizing the container.
-       see here: https://github.com/vercel/next.js/issues/18915 */}
-      <Menu.Button className="block h-[28px]">{button}</Menu.Button>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="uppercase text-marble-white text-lg absolute left-[-5px] mt-[9px] origin-top-right bg-tunnel-black border border-[#646464] focus:outline-none whitespace-nowrap z-10">
-          {items.map((item) => (
-            <Menu.Item key={item.name}>
-              {({ active }) => (
-                <a
-                  className={`${
-                    active ? "" : "text-gray-900"
-                  } group flex rounded-md items-center w-full px-2 py-2`}
-                  href={item.href}
-                >
-                  {item.name}
-                </a>
-              )}
-            </Menu.Item>
-          ))}
-        </Menu.Items>
-      </Transition>
+      {({ open }) => (
+        <>
+          <Menu.Button className="block h-[28px] text-marble-white">
+            <div className="flex items-center">
+              {button} <Chevron isUp={open} className="ml-2" />
+            </div>
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              className={`uppercase text-marble-white text-lg absolute ${side}-[-5px] mt-[9px] origin-top-right bg-tunnel-black border border-[#646464] focus:outline-none whitespace-nowrap z-10`}
+            >
+              {items.map((item) => (
+                <Menu.Item key={item.name}>
+                  {({ active }) => (
+                    <a
+                      className={`${
+                        active && "text-neon-blue"
+                      } group flex rounded-md items-center w-full px-4 py-2`}
+                      href={item.href}
+                    >
+                      {item.name}
+                    </a>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
     </Menu>
   )
 }
--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,9 +1,6 @@
 import { Head, BlitzLayout } from "blitz"
-import { Image } from "blitz"
 import Ticker from "../components/Ticker"
-import Dropdown from "../components/Dropdown"
-import logo from "../../../public/station_logo.svg"
-import SoundIcon from "../icons/soundIcon"
+import Navigation from "../components/Navigation"
 
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   return (
@@ -12,31 +9,7 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
         <title>{title || "station-web"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* what will eventually become the nav */}
-      <div className="h-12 w-full px-4 bg-tunnel-black flex flex-row justify-between border-b border-b-concrete">
-        <Dropdown
-          className="self-center"
-          button={<Image src={logo} alt="Station logo, the letters station spelled out." />}
-          items={[
-            { name: "newstand", href: "/newstand" },
-            { name: "contact the staff", href: "/contact" },
-            { name: "discord", href: "/discord" },
-            { name: "twitter", href: "/twitter" },
-          ]}
-        />
-
-        <div className="flex items-center">
-          <span className="p-4 border-l border-l-concrete uppercase text-marble-white text-lg">
-            Map
-          </span>
-          <span className="p-4 border-l border-l-concrete block">
-            <SoundIcon />
-          </span>
-          <span className="p-4 pr-0 uppercase text-magic-mint text-lg border-l border-l-concrete">
-            Enter Station
-          </span>
-        </div>
-      </div>
+      <Navigation user={{ address: "0xMCG" }} />
       <Ticker />
       {children}
     </>
