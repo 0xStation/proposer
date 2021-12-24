4fc94790c117997bd36044395479cec44ffbc108
Michael Gingras <mcg79@cornell.edu>
2021-12-23 23:01:57 -0500

    feat: app core layout. Adding new routes, hover states, cleaning up slightly

--- app/pages/terminal/[terminalName]/initiative.tsx
+++ app/pages/terminal/[terminalName]/initiative.tsx
@@ -3,10 +3,10 @@ import Layout from "app/core/layouts/Layout"
 import TerminalNavigation from "app/terminal/components/Navigation"
 import InitiativeCard from "app/terminal/components/InitiativeCard"
 
-const TerminalPage: BlitzPage = () => {
+const TerminalInitiativePage: BlitzPage = () => {
   return (
     <TerminalNavigation>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         <InitiativeCard title="protocol v1" description="ok" contributors={[]} />
         <InitiativeCard title="protocol v1" description="ok" contributors={[]} />
         <InitiativeCard title="protocol v1" description="ok" contributors={[]} />
@@ -15,7 +15,7 @@ const TerminalPage: BlitzPage = () => {
   )
 }
 
-TerminalPage.suppressFirstRenderFlicker = true
-TerminalPage.getLayout = (page) => <Layout title="Terminal">{page}</Layout>
+TerminalInitiativePage.suppressFirstRenderFlicker = true
+TerminalInitiativePage.getLayout = (page) => <Layout title="Initiatives">{page}</Layout>
 
-export default TerminalPage
+export default TerminalInitiativePage
