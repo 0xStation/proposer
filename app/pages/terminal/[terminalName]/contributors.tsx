ba0a604ad1c1300466a180bd2e37342f8e188126
Michael Gingras <mcg79@cornell.edu>
2021-12-24 21:21:21 -0500

    feat: modal component

--- app/pages/terminal/[terminalName]/contributors.tsx
+++ app/pages/terminal/[terminalName]/contributors.tsx
@@ -1,10 +1,34 @@
 import { BlitzPage } from "blitz"
+import { useState } from "react"
 import Layout from "app/core/layouts/Layout"
 import TerminalNavigation from "app/terminal/components/Navigation"
-import InitiativeCard from "app/terminal/components/InitiativeCard"
+import Modal from "../../../core/components/Modal"
 
 const TerminalContributorsPage: BlitzPage = () => {
-  return <TerminalNavigation />
+  let [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <Modal
+        title="test"
+        subtitle="im just testing this darn thing"
+        open={isOpen}
+        toggle={setIsOpen}
+      >
+        <div className="text-marble-white">contents</div>
+      </Modal>
+      <TerminalNavigation>
+        <button
+          className="bg-magic-mint text-tunnel-black px-2 py-1"
+          onClick={() => {
+            setIsOpen(true)
+          }}
+        >
+          Open Modal
+        </button>
+      </TerminalNavigation>
+    </>
+  )
 }
 
 TerminalContributorsPage.suppressFirstRenderFlicker = true
