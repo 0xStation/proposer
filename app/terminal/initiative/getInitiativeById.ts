dfe030e4fb3817c1961a317f133af81de02ab166
Abenazer Mekete <abenazer101@gmail.com>
2021-12-29 22:46:52 -0600

    [ignore] changed my git configm email for vercel

--- app/terminal/initiative/getInitiativeById.ts
+++ app/terminal/initiative/getInitiativeById.ts
@@ -1,6 +1,21 @@
 import db from "db"
 import * as z from "zod"
 
+// type Initiative {
+// 	id: String
+// 	terminalId: String
+// 	localId: Number // on-chain integer to identify within terminal's graph
+// 	applications: InitiativeApplication[]
+// 	openings: Number
+// 	name: String
+// 	shortName: String
+// 	description: String
+// 	links: JSON // [{type: "", url: ""}, ...]
+// 	banner: String
+// 	contributeText: String
+// 	rewardText: String
+// }
+
 const GetInitiativeById = z.object({
   id: z.number(),
 })
