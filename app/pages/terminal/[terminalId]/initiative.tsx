86cc14fa878c266b029c7a63fffd616b7dbb9a3b
Michael Gingras <mcg79@cornell.edu>
2022-01-05 19:46:27 -0700

    adding optional chaining

--- app/pages/terminal/[terminalId]/initiative.tsx
+++ app/pages/terminal/[terminalId]/initiative.tsx
@@ -23,8 +23,8 @@ const TerminalInitiativePage: BlitzPage = () => {
             return (
               <InitiativeCard
                 key={initiative.id}
-                title={initiative.data.name}
-                description={initiative.data.description}
+                title={initiative?.data?.name || "Title"}
+                description={initiative?.data?.description || "Description"}
                 contributors={[]}
               />
             )
