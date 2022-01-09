76b5c908d104d9e417c2b79e55d1da779f3758c6
Conner Swenberg <38736612+symmtry@users.noreply.github.com>
2022-01-08 18:51:13 -0800

    Capitalize names and add ahs (#26)

--- app/core/utils/data.js
+++ app/core/utils/data.js
@@ -2,63 +2,68 @@
 // nft number will call the respective smart contract
 export const users = {
   "0x016562aA41A8697720ce0943F003141f5dEAe006": {
-    fname: "conner",
+    fname: "Conner",
     handle: "symmtry",
     nftId: 0,
   },
   "0x65A3870F48B5237f27f674Ec42eA1E017E111D63": {
-    fname: "michael",
+    fname: "Michael",
     handle: "0xmcg",
     nftId: 1,
   },
   "0xd32FA3e71737a19eE4CA44334b9f3c52665a6CDB": {
-    fname: "mind",
+    fname: "Mind",
     handle: "mindapi",
     nftId: 2,
   },
   "0x78918036a8e4B9179bEE3CAB57110A3397986E44": {
-    fname: "tina",
+    fname: "Tina",
     handle: "fakepixels",
     nftId: 3,
   },
   "0x17B7163E708A06De4DdA746266277470dd42C53f": {
-    fname: "brendan",
+    fname: "Brendan",
     handle: "brendo",
     nftId: 4,
   },
   "0xB0F0bA31aA582726E36Dc0c79708E9e072455eD2": {
-    fname: "calvin",
+    fname: "Calvin",
     handle: "cchengasaurus",
     nftId: 6,
   },
   "0xaE55f61f85935BBB68b8809d5c02142e4CbA9a13": {
-    fname: "kristen",
+    fname: "Kristen",
     handle: "rie",
     nftId: 7,
   },
   "0x8FAA5498Ca6fc9A61BA967E07fBc9420aab99E55": {
-    fname: "akshay",
+    fname: "Akshay",
     handle: "wagmiking",
     nftId: 9,
   },
   "0x32447704a3ac5ed491b6091497ffb67a7733b624": {
-    fname: "alli",
+    fname: "Alli",
     handle: "sonofalli",
     nftId: 10,
   },
   "0x90A0233A0c27D15ffA23E293EC8dd6f2Ef2942e2": {
-    fname: "kassen",
+    fname: "Kassen",
     handle: "kassenq",
     nftId: 11,
   },
   "0x237c9dbB180C4Fbc7A8DBfd2b70A9aab2518A33f": {
-    fname: "abenazer",
+    fname: "Abe",
     handle: "cryptoabe",
     nftId: 12,
   },
   "0x2f40e3Fb0e892240E3cd5682D10ce1860275174C": {
-    fname: "nick",
+    fname: "Nick",
     handle: "zy2",
     nftId: 13,
+  },
+  "0x69F35Bed06115Dd05AB5452058d9dbe8a7AD80f1": {
+    fname: "Alex",
+    handle: "ahs",
+    nftId: 14,
   },
 }
