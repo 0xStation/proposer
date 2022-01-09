cf323300868d3640ab3a7205dd84077b01817f53
Conner Swenberg <cls364@cornell.edu>
2022-01-09 13:59:53 -0800

    Update seed scripts with logs

--- db/seeds.ts
+++ db/seeds.ts
@@ -6,8 +6,11 @@ import { seedInitiatives } from "./seed/initiatives"
  * This seed function is executed when you run `blitz db seed`.
  */
 const seed = async () => {
+  console.log("Seeding terminals...")
   const terminals = await seedTerminals()
+  console.log("Seeding initiatives...")
   await seedInitiatives(terminals)
+  console.log("Seeding contributors...")
   await seedContributors()
 }
 
