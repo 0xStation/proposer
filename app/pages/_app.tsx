4c2d9b2a2bed92d0d3289998e68a9e37eaa2865d
kristencheung <krcheung9@gmail.com>
2022-01-11 18:05:10 -0800

    spike to test disconnecting wallet

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,14 +1,23 @@
 import { AppProps } from "blitz"
 import { Rinkeby, Mainnet, Localhost, DAppProvider, Config } from "@usedapp/core"
 import "app/core/styles/index.css"
+import { useSuppressFirstRenderFlicker } from "app/core/hooks/useSuppressFirstRenderFlicker"
 
 const config: Config = {
+  readOnlyChainId: Rinkeby.chainId,
+  readOnlyUrls: {
+    [Rinkeby.chainId]: "https://eth-rinkeby.alchemyapi.io/v2/ZJIj3ytrFaWioVp550EyzD4MR9q5VClg",
+  },
   // supported networks
   networks: [Localhost, Mainnet, Rinkeby],
 }
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
+
+  const suppressRender = useSuppressFirstRenderFlicker()
+
+  if (suppressRender) return null
 
   return <DAppProvider config={config}>{getLayout(<Component {...pageProps} />)}</DAppProvider>
 }
