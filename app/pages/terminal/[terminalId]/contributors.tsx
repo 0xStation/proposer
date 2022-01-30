d936b666d8a6448ed5bf37f16e334e126ef5dd8a
Abenazer Mekete <abenazer101@gmail.com>
2022-01-29 18:37:40 -0600

    clean up code base

--- app/pages/terminal/[terminalId]/contributors.tsx
+++ app/pages/terminal/[terminalId]/contributors.tsx
@@ -3,11 +3,9 @@ import { BlitzPage, useQuery, useParam, invoke } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import TerminalNavigation from "app/terminal/components/Navigation"
 import getAccountsByRole from "app/account/queries/getAccountsByRole"
-// import ContributorCard from "app/core/components/ContributorCard"
 import { Account } from "app/account/types"
 import EndorseContributorModal from "app/contributors/components/EndorseContributorModal"
 import { Pill } from "app/core/components/Pill"
-import useStore from "app/core/hooks/useStore"
 import { TalentIdentityUnit as ContributorCard } from "app/core/components/TalentIdentityUnit/index"
 
 const TerminalContributorsPage: BlitzPage = () => {
@@ -15,20 +13,10 @@ const TerminalContributorsPage: BlitzPage = () => {
   const [selectedUserToEndorse, setSelectedUserToEndorse] = useState<Account | null>(null)
   const [selectedRole, setRole] = useState("STAFF")
   const [selectedContributors, setSelectedContributors] = useState<Account[] | null>(null)
-  const openEndorseModal = () => setEndorseModalIsOpen(!endorseModalIsOpen)
-  const activeUser: Account | null = useStore((state) => state.activeUser)
-
-  const accepted = true
-  const endorse = false
 
   const roles = ["STAFF", "COMMUTER", "VISITOR"]
 
   useEffect(() => {
-    // if (!allContributors){
-    //   setSelectedContributors(null)
-    // } else {
-    //   const roleContributors =  allContributors.forEach(sorted(selectedRole))
-    // }
     if (selectedRole) {
       const getApplicationsFromInitiative = async () => {
         let contributors = await invoke(getAccountsByRole, { role: selectedRole })
@@ -88,19 +76,6 @@ const TerminalContributorsPage: BlitzPage = () => {
                 )
               })}
             </div>
-            {/* <div className="flex-auto text-marble-white">
-            {!applications || !applications.length ? (
-              <div>
-                {selectedInitiative ? (
-                  <div>There are no active applications for this initiative.</div>
-                ) : (
-                  <div>Please select an initiative to view applications.</div>
-                )}
-              </div>
-            ) : (
-              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">{applicationCards}</div>
-            )}
-          </div> */}
             {!selectedContributors || !selectedContributors.length ? (
               <div className="text-marble-white">
                 {selectedRole ? (
@@ -112,32 +87,6 @@ const TerminalContributorsPage: BlitzPage = () => {
             ) : (
               <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">{contributorCards}</div>
             )}
-            {/* {contributors.length ? (
-              <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {contributors.length &&
-                  contributors
-                    .filter(
-                      (contributor) =>
-                        typeof contributor.data.ticketId === "number" &&
-                        contributor.data.ticketId >= 0
-                    )
-                    .map((contributor, index) => (
-                      <ContributorCard
-                        key={index}
-                        contributor={contributor as Account}
-                        endorse={endorse}
-                        accepted={accepted}
-                        openEndorseModal={openEndorseModal}
-                        setSelectedUserToEndorse={setSelectedUserToEndorse}
-                        activeUser={activeUser}
-                      />
-                    ))}
-              </div>
-            ) : (
-              <div className="text-marble-white">
-                There are no {selectedRole.toLowerCase()}s in this terminal.
-              </div>
-            )} */}
           </div>
         </>
       ) : (
