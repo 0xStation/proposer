5433ac5d89f12787f7676f3e2dbb4b4257698ac8
Michael Gingras <mcg79@cornell.edu>
2022-02-02 10:39:48 -0700

    card header size

--- app/initiative/components/InitiativeCard.tsx
+++ app/initiative/components/InitiativeCard.tsx
@@ -1,7 +1,7 @@
 const InitiativeCard = ({ title, description, contributors }) => {
   return (
     <div className="border border-concrete p-4 flex flex-col cursor-pointer h-full hover:border-marble-white">
-      <h3 className="text-marble-white text-2xl">{title}</h3>
+      <h3 className="text-marble-white text-lg">{title}</h3>
       <p className="text-marble-white text-base mt-2 grow">{description}</p>
       <div className="mt-8 flex flex-row">
         <span className="h-4 w-4 rounded-full bg-concrete border border-marble-white block"></span>
