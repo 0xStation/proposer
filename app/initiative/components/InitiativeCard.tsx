6c7fc15001b54775421a88456efd64d994aab1d8
Abenazer Mekete <abenazer101@gmail.com>
2022-02-02 15:39:24 -0600

    added calling for contributors (#105)
    Co-authored-by: conner <38736612+symmtry@users.noreply.github.com>

--- app/initiative/components/InitiativeCard.tsx
+++ app/initiative/components/InitiativeCard.tsx
@@ -1,39 +1,46 @@
 import { useQuery } from "blitz"
 import getAccountsByAddresses from "app/account/queries/getAccountsByAddresses"
 
-const InitiativeCard = ({ title, description, contributors }) => {
+const InitiativeCard = ({ title, description, contributors, isAcceptingApplications }) => {
   return (
     <div className="border border-concrete p-4 flex flex-col cursor-pointer h-full hover:border-marble-white">
-      <h3 className="text-marble-white text-lg">{title}</h3>
-      <p className="text-marble-white text-base mt-2 grow">{description}</p>
-      <div className="mt-8 flex flex-row">
-        {contributors?.length
-          ? contributors.slice(0, 5).map(({ data: { pfpURL } }, idx) => {
-              const pfpStyling = "h-6 w-6 rounded-full border block border-marble-white"
-              const nestedStyling = idx ? "ml-[-5px]" : ""
-              if (idx === 4) {
-                const additionalContributors = (contributors?.length || 0) - 4
-                return (
+      <h3 className="text-marble-white text-2xl">{title}</h3>
+      <p className="text-marble-white text-sm mt-2 grow">{description}</p>
+      <div className="flex flex-row space-x-3">
+        <div className="mt-8 flex flex-row flex-auto">
+          {contributors?.length
+            ? contributors.slice(0, 5).map(({ data: { pfpURL } }, idx) => {
+                const pfpStyling = "h-6 w-6 rounded-full border block border-marble-white"
+                const nestedStyling = idx ? "ml-[-5px]" : ""
+                if (idx === 4) {
+                  const additionalContributors = (contributors?.length || 0) - 4
+                  return (
+                    <span
+                      key={idx}
+                      className={`bg-neon-blue text-marble-white text-[10px] text-center items-center pt-1 ${pfpStyling} ${nestedStyling}`}
+                    >
+                      {additionalContributors}+
+                    </span>
+                  )
+                }
+                let pfpBubble = pfpURL ? (
                   <span
                     key={idx}
-                    className={`bg-neon-blue text-marble-white text-[10px] text-center items-center pt-1 ${pfpStyling} ${nestedStyling}`}
-                  >
-                    {additionalContributors}+
-                  </span>
+                    className={`bg-contain bg-clip-padding ${pfpStyling} ${nestedStyling}`}
+                    style={{ backgroundImage: `url(${pfpURL})` }}
+                  ></span>
+                ) : (
+                  <span key={idx} className={`bg-concrete ${pfpStyling} ${nestedStyling}`}></span>
                 )
-              }
-              let pfpBubble = pfpURL ? (
-                <span
-                  key={idx}
-                  className={`bg-contain bg-clip-padding ${pfpStyling} ${nestedStyling}`}
-                  style={{ backgroundImage: `url(${pfpURL})` }}
-                ></span>
-              ) : (
-                <span key={idx} className={`bg-concrete ${pfpStyling} ${nestedStyling}`}></span>
-              )
-              return pfpBubble
-            })
-          : "N/A"}
+                return pfpBubble
+              })
+            : "N/A"}
+        </div>
+        {isAcceptingApplications && (
+          <div className="flex-auto grid place-content-end">
+            <span className="text-magic-mint text-xs">CALLING FOR CONTRIBUTORS</span>
+          </div>
+        )}
       </div>
     </div>
   )
