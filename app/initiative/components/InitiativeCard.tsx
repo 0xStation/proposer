4169335f693f73d9d5acf335159a9490ca01bdf7
kristencheung <krcheung9@gmail.com>
2022-02-02 13:38:13 -0800

    is it bold, is is lg, we want to know

--- app/initiative/components/InitiativeCard.tsx
+++ app/initiative/components/InitiativeCard.tsx
@@ -4,7 +4,7 @@ import getAccountsByAddresses from "app/account/queries/getAccountsByAddresses"
 const InitiativeCard = ({ title, description, contributors }) => {
   return (
     <div className="border border-concrete p-4 flex flex-col cursor-pointer h-full hover:border-marble-white">
-      <h3 className="text-marble-white text-lg font-bold">{title}</h3>
+      <h3 className="text-marble-white text-2xl font-bold">{title}</h3>
       <p className="text-marble-white text-base mt-2 grow">{description}</p>
       <div className="mt-8 flex flex-row">
         {contributors?.length
