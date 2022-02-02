e7ef9a28913b16a655f52c0c8881f36838262330
Michael Gingras <mcg79@cornell.edu>
2022-02-01 21:24:34 -0700

    and again

--- app/api/demo.ts
+++ app/api/demo.ts
@@ -1,10 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import aws from "aws-sdk"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log("just trying to view the processes")
-  res.json({
+  const s3 = new aws.S3({
     endpoint: process.env.SPACES_ENDPOINT,
     accessKeyId: process.env.SPACES_ACCESS_KEY_ID,
     secretAccessKey: process.env.SPACES_SECRET_KEY,
+  })
+
+  console.log("just trying to view the processes")
+  res.json({
+    s3: s3,
   })
 }
