3d1c37c259dfb2d0f61055f277ea5075e4553497
Michael Gingras <mcg79@cornell.edu>
2022-02-02 18:55:34 -0700

    debugging the rinkby

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,39 +1,38 @@
-import { useState, useEffect } from "react"
 import { Head, BlitzLayout } from "blitz"
 import Ticker from "../components/Ticker"
 import Navigation from "../components/Navigation"
 import ModalContainer from "../components/ModalContainer"
-import { useAccount } from "wagmi"
+import { useNetwork } from "wagmi"
 
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
-  const [chainId, setChainId] = useState<number>()
-  const [{ data: accountData }] = useAccount()
+  const [{ data: chainData }, switchNetwork] = useNetwork()
 
-  const getChainId = async () => {
-    const chainId = await accountData?.connector?.getChainId()
-    setChainId(chainId)
-  }
-
-  useEffect(() => {
-    getChainId()
-  }, [accountData])
   return (
     <>
       <Head>
         <title>{title || "Station"}</title>
         <link rel="icon" href="/station-logo-favicon.ico" />
       </Head>
-      {chainId !== 4 ? (
+      {chainData?.chain?.id !== undefined && chainData?.chain?.id !== 4 ? (
         <>
           <Navigation />
           <Ticker />
           <div className="flex items-center justify-center min-h-[calc(100vh-15rem)]">
             <div className="text-marble-white text-center">
               <h1 className="text-4xl">Incorrect Network</h1>
-              <p className="max-w-lg mt-2">
+              <p className="max-w-lg mt-4">
                 Currently, the Station network only supports Rinkby. Please switch your connection
                 to the Rinkby chain.
               </p>
+
+              {switchNetwork && (
+                <button
+                  className="rounded-lg px-2 py-1 mt-4 bg-magic-mint text-tunnel-black"
+                  onClick={() => switchNetwork(4)}
+                >
+                  Switch to Rinkby
+                </button>
+              )}
             </div>
           </div>
         </>
