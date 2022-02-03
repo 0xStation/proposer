2097a0b791fd5f8b8839776e451e237de613c7c6
conner <38736612+symmtry@users.noreply.github.com>
2022-02-03 14:29:48 -0800

    Seed skills (#142)

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,18 +1,12 @@
-import { seedContributors } from "./seed/contributors"
 import { seedSkills } from "./seed/skills"
-import { seedApplications } from "./seed/applications"
 import { seed as seedStationTerminal } from "./seed/station/0-terminal"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
  */
 const seed = async () => {
-  // console.log("Seeding contributors...")
-  // await seedContributors(terminals)
-  // console.log("Seeding skills")
-  // await seedSkills()
-  // console.log("Seeding applications")
-  // await seedApplications()
+  console.log("Seeding skills")
+  await seedSkills()
   console.log("Seeding station terminal...")
   await seedStationTerminal()
 }
