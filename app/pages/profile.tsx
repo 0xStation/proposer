4a85229a08811146858faf4d2bb58c159b2b4b0f
Michael Gingras <mcg79@cornell.edu>
2022-02-16 23:01:34 -0700

    scaffolding profile page

---
+++ app/pages/profile.tsx
@@ -0,0 +1,92 @@
+import { useMemo } from "react"
+import { BlitzPage } from "blitz"
+import { useAccount } from "wagmi"
+import Layout from "app/core/layouts/Layout"
+import useStore from "app/core/hooks/useStore"
+import { Account } from "app/account/types"
+import { getWalletString } from "app/utils/getWalletString"
+
+// the profile homepage
+// can see a users initiatives + termials + profile info at a glance
+const ProfileHome: BlitzPage = () => {
+  const [{ data: accountData }] = useAccount({
+    fetchEns: true,
+  })
+  const address = useMemo(() => accountData?.address || undefined, [accountData?.address])
+  const activeUser: Account | null = useStore((state) => state.activeUser)
+
+  console.log(activeUser)
+
+  // todo: return something better
+  if (!activeUser) {
+    return <></>
+  }
+
+  return (
+    <div
+      className="w-full h-full bg-cover bg-center bg-no-repeat border"
+      style={{ backgroundImage: "url('/station-cover.png')" }}
+    >
+      <div className="bg-tunnel-black min-h-[calc(100vh-15rem)] h-[1px] mt-36 relative">
+        <div className="grid gap-0 grid-cols-1 md:grid-cols-3 xl:grid-cols-4 max-w-screen-xl h-full mx-auto">
+          <div className="col-span-1 pl-4 text-2xl border-concrete border-b pb-12 md:border-b-0 md:border-r md:pr-6 h-full">
+            <div className="border-b border-concrete pb-4">
+              <div className="flex items-center mt-12">
+                <div className="flex-2/5 mr-4">
+                  <img
+                    src={activeUser.data.pfpURL}
+                    alt="PFP"
+                    className="h-[52px] w-[52px] border border-marble-white rounded-full"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <h1 className="text-2xl text-marble-white">{activeUser.data.name}</h1>
+                  <span className="text-base text-concrete">
+                    {getWalletString(activeUser.address)}
+                  </span>
+                </div>
+              </div>
+              <div className="mt-4 space-y-4">
+                <div className="flex flex-col">
+                  <h3 className="text-marble-white text-base font-bold">Skills</h3>
+                  {/* {activeUser.skills.map((skill) => {
+                    return <div className="text-marble-white">{skill.name}</div>
+                  })} */}
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="text-marble-white text-base font-bold">Timezone</h3>
+                  <span className="text-marble-white text-base">{activeUser.data.timezone}</span>
+                </div>
+                <div className="flex flex-col">
+                  <h3 className="text-marble-white text-base font-bold">Contact</h3>
+                  <span className="text-marble-white text-base">{activeUser.data.discordId}</span>
+                </div>
+                <button className="border border-marble-white text-marble-white text-base w-full rounded-md hover:text-tunnel-black hover:bg-marble-white">
+                  Edit Profile
+                </button>
+              </div>
+            </div>
+            <ul className="mt-4 text-lg">
+              <li className={`cursor-pointer hover:text-marble-white text-marble-white font-bold`}>
+                Terminals
+              </li>
+              <li className={`cursor-pointer hover:text-marble-white text-concrete font-bold`}>
+                Initiatives
+              </li>
+            </ul>
+          </div>
+          <div className="col-span-2 xl:col-span-3 px-6 pb-12">
+            <div className="mt-12">
+              <div className="text-marble-white">stuff goes here</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ProfileHome.suppressFirstRenderFlicker = true
+ProfileHome.getLayout = (page) => <Layout title="My Station Profile">{page}</Layout>
+
+export default ProfileHome
---
+++ app/pages/profile/edit.tsx
@@ -0,0 +1,59 @@
+import { useMemo } from "react"
+import { BlitzPage } from "blitz"
+import { useAccount } from "wagmi"
+import AccountForm from "app/account/components/AccountForm"
+import Layout from "app/core/layouts/Layout"
+import useStore from "app/core/hooks/useStore"
+import { Account } from "app/account/types"
+
+// todo: make this edit and not create
+// just scaffolding pages for now :p
+const EditProfile: BlitzPage = () => {
+  const [{ data: accountData }] = useAccount({
+    fetchEns: true,
+  })
+  const address = useMemo(() => accountData?.address || undefined, [accountData?.address])
+  const activeUser: Account | null = useStore((state) => state.activeUser)
+
+  if (!address) {
+    return (
+      <div className="mx-auto max-w-2xl py-12">
+        <h1 className="text-marble-white text-3xl text-center">
+          Your wallet needs to be connected to create an account
+        </h1>
+      </div>
+    )
+  }
+
+  // todo: make edit a link
+  // would be cool to have a better error state
+  if (activeUser) {
+    return (
+      <div className="mx-auto max-w-2xl py-12">
+        <h1 className="text-marble-white text-3xl text-center">Your already have a profile!</h1>
+        <p className="text-marble-white text-center mt-2">
+          Do you want to edit your profile instead?
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div
+      className="w-full h-full bg-cover bg-center bg-no-repeat border"
+      style={{ backgroundImage: "url('/station-cover.png')" }}
+    >
+      <div className="bg-tunnel-black min-h-[calc(100vh-15rem)] h-[1px] mt-36 relative">
+        <h1 className="text-marble-white text-3xl text-center pt-12">Complete your profile</h1>
+        <div className="mx-auto max-w-2xl pb-12">
+          <AccountForm onSuccess={() => console.log("done")} address={address} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+EditProfile.suppressFirstRenderFlicker = true
+EditProfile.getLayout = (page) => <Layout title="Edit Profile">{page}</Layout>
+
+export default EditProfile
