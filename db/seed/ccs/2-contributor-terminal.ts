85bc1a74158dcba7fdb0c0217fda7dcefc4f975e
conner <38736612+symmtry@users.noreply.github.com>
2022-02-18 18:58:15 -0800

    Seed ccs terminal (#171)
    * Add initial CCS seed scripts
    
    * Add CCS seed files

---
+++ db/seed/ccs/2-contributor-terminal.ts
@@ -0,0 +1,122 @@
+import db from "../../index"
+import { terminalId, roleIds } from "./0-terminal"
+
+interface ContributorSeed {
+  address: string
+  joinedAt: Date
+  role: number
+}
+
+const bhaumik: ContributorSeed = {
+  address: "0xc7814D61A8236303a8854fc272cA0419A7e18E32",
+  joinedAt: new Date("2021-09-01"),
+  role: roleIds.staff,
+}
+const melvin: ContributorSeed = {
+  address: "0x358a7107fB497d96F406E8D807acC32AEe1d889a",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const nick: ContributorSeed = {
+  address: "0x8cD48c84FFc642653f34dF82dd7ac9541A4dead9",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const liam: ContributorSeed = {
+  address: "0xad9242795ad7fe630adda2b29ed39a4f4c4fd84b",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const tasha: ContributorSeed = {
+  address: "0x0EF7fc7B6730148Af0b35e0754a1420Bad088c4E",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const courtland: ContributorSeed = {
+  address: "0xfa62b9143f6b0d7e125f4e721a613a7313948c75",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const thomas: ContributorSeed = {
+  address: "0x9804C778D23c83ea2c7981f6cf5e3f299AEBEb9e",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const olly: ContributorSeed = {
+  address: "0xB15F50e5AD35F29Bf52601d6629b3De44a3b922e",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const kemi: ContributorSeed = {
+  address: "0x3865d06B16BF8defC9795881F7a08D12ffbAaB99",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const caryn: ContributorSeed = {
+  address: "0x7D65B0A423c75e531BE681750514ca73Ff8B1d6A",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const kassen: ContributorSeed = {
+  address: "0x90A0233A0c27D15ffA23E293EC8dd6f2Ef2942e2",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+const sara: ContributorSeed = {
+  address: "0xa139d29Cf66171d83E816D7b1dF768e07c9BC6Ab",
+  joinedAt: new Date("2022-02-01"),
+  role: roleIds.teamLead,
+}
+
+export const ccsContributors = {
+  bhaumik,
+  melvin,
+  nick,
+  liam,
+  tasha,
+  courtland,
+  thomas,
+  olly,
+  kemi,
+  caryn,
+  kassen,
+  sara,
+}
+
+export async function seed() {
+  for (const name in ccsContributors) {
+    const contributorSeed = ccsContributors[name]! as ContributorSeed
+
+    console.log(`  ${contributorSeed.address}`)
+
+    const account = await db.account.findUnique({ where: { address: contributorSeed.address } })
+
+    if (!account) {
+      console.log(`no account found with name ${name}, address ${contributorSeed.address}`)
+      continue
+    }
+
+    const accountTerminal = await db.accountTerminal.upsert({
+      where: {
+        accountId_terminalId: {
+          accountId: account?.id,
+          terminalId,
+        },
+      },
+      create: {
+        accountId: account.id,
+        terminalId,
+        roleLocalId: contributorSeed.role,
+        joinedAt: contributorSeed.joinedAt,
+      },
+      update: {
+        roleLocalId: contributorSeed.role,
+        joinedAt: contributorSeed.joinedAt,
+      },
+    })
+
+    console.log(
+      `Contributor updated: accountId ${accountTerminal.accountId}, role ${accountTerminal.roleLocalId}, joinedAt ${accountTerminal.joinedAt}`
+    )
+  }
+}
--- db/steward.ts
+++ db/steward.ts
@@ -3,14 +3,23 @@ import { seed as seedStationTerminal } from "./seed/station/0-terminal"
 import { seed as seedStationTerminalContributors } from "./seed/station/1-contributor-terminal"
 import { seed as seedStationInitiativeContributors } from "./seed/station/2-contributor-initiative"
 import { seed as seedStationContributorDetails } from "./seed/station/3-contributor-details"
+import { seed as seedCCSTerminal } from "./seed/ccs/0-terminal"
+import { seed as seedCCSContributors } from "./seed/ccs/1-contributors"
+import { seed as seedCCSTerminalContributors } from "./seed/ccs/2-contributor-terminal"
+import { seed as seedCCSInitiativeContributors } from "./seed/ccs/3-contributor-initiatives"
+// import { seed as seedCCSInitiativeContributors } from "./seed/ccs/2-contributor-initiative"
+// import { seed as seedCCSContributorDetails } from "./seed/ccs/3-contributor-details"
 
 // This seed function is executed when you run `blitz db seed -f db/steward.ts`.
 const seed = async () => {
   // //// PHASE 1
   // console.log("Seeding skills...")
   // await seedSkills()
-  // console.log("Seeding station terminal...")
-  // await seedStationTerminal()
+  console.log("Seeding CCS terminal...")
+  // await seedCCSTerminal()
+  // await seedCCSContributors()
+  // await seedCCSTerminalContributors()
+  await seedCCSInitiativeContributors()
   // //// PHASE 2
   // console.log("seeding terminal contributors...")
   // await seedStationTerminalContributors()
