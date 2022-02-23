b1332098daf79e0ca5cc2c8d62e64d6546b110b8
kristencheung <krcheung9@gmail.com>
2022-02-23 15:03:52 -0700

    fetch terminals from db, not from subgraph for navigation map

--- app/terminal/queries/getTerminalsByAccount.ts
+++ app/terminal/queries/getTerminalsByAccount.ts
@@ -1,43 +1,22 @@
 import db from "db"
 import * as z from "zod"
-import { request, gql } from "graphql-request"
-import { Terminal } from "../types"
 
 const GetTerminalsByAccount = z.object({
-  address: z.string().optional(),
+  accountId: z.number(),
 })
 
 export default async function getTerminalsByAccount(input: z.infer<typeof GetTerminalsByAccount>) {
   const params = GetTerminalsByAccount.parse(input)
+  const tickets = await db.accountTerminal.findMany({
+    include: {
+      terminal: true,
+    },
+    where: {
+      accountId: params.accountId,
+    },
+  })
 
-  if (params.address === undefined) {
-    return [] as Terminal[]
-  }
-
-  let FETCH_USERS_TICKETS_QUERY = gql`
-  {
-    tickets(where: { owner: "${params.address}" }) {
-      terminal {
-        tokenAddress
-      }
-    }
-  }
-`
-  // need to look into subgraph to see which terminals a particular address is a part of
-  let data = await request(
-    "https://api.thegraph.com/subgraphs/name/akshaymahajans/stationtest",
-    FETCH_USERS_TICKETS_QUERY
-  )
-
-  // the subgraph does not return full terminal information, for that, we need the web2 database
-  // we can gather the tokenAddresses, which are stored in the web2 database, in order to pull
-  // full records
-  const tokenAddressesOfUsersTerminals = data.tickets.map((ticket) => ticket.terminal.tokenAddress)
-
-  // grab the full terminal inforamtion based on the terminal tokenAddresses we got in the previous step
-  const terminals = (await db.terminal.findMany({
-    where: { ticketAddress: { in: tokenAddressesOfUsersTerminals } },
-  })) as Terminal[]
-
-  return terminals
+  return tickets.map((ticket) => ({
+    ...ticket.terminal,
+  }))
 }
