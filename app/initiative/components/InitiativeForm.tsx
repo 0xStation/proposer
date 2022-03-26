df8cf12930907742937c6822308e7ae599abb472
kristencheung <krcheung9@gmail.com>
2022-03-25 17:13:47 -0700

    fix commas as newline issue

--- app/initiative/components/InitiativeForm.tsx
+++ app/initiative/components/InitiativeForm.tsx
@@ -11,8 +11,8 @@ interface InitiativeParams {
   name: string
   oneLiner: string
   commitment: string
-  rewardText: string[] | string
-  contributeText: string[] | string
+  rewardText: string
+  contributeText: string
   links?: {
     url: string
     symbol: number
@@ -43,12 +43,18 @@ const InitiativeForm = ({
     if (Array.isArray(text)) {
       return text
     }
-    return text.split(",")
+    return text.split("\n")
+  }
+
+  const initialFormValues = {
+    ...initiative?.data,
+    contributeText: initiative?.data?.contributeText?.join("\n"),
+    rewardText: initiative?.data?.rewardText?.join("\n"),
   }
 
   return (
     <Form
-      initialValues={initiative?.data || {}}
+      initialValues={initialFormValues || {}}
       onSubmit={async (values: InitiativeParams) => {
         try {
           if (isEdit) {
@@ -67,14 +73,6 @@ const InitiativeForm = ({
       render={({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-2 gap-y-6 gap-x-2">
-            {/* <div className="flex flex-col col-span-2">
-              <div className="mt-10 mb-9">
-                <h1 className="font-bold text-2xl">Initiative Info</h1>
-                <p className="mt-3">
-                  Provide details on your initiative to help prospective applicants learn more.
-                </p>
-              </div>
-            </div> */}
             <div className="flex flex-col col-span-2">
               <label htmlFor="name" className="text-marble-white text-base font-bold">
                 Initiative Name
@@ -101,13 +99,12 @@ const InitiativeForm = ({
               <label htmlFor="contactURL" className="text-marble-white text-base font-bold">
                 Full Description
               </label>
-              <p className="text-concrete text-sm mb-2">Separate paragraphs by comma</p>
               <div className="flex flex-row mt-1">
                 <Field
                   component="textarea"
                   name="contributeText"
                   placeholder="Contribute text"
-                  className="border border-concrete bg-wet-concrete text-marble-white p-2 flex-1  h-36"
+                  className="border border-concrete bg-wet-concrete text-marble-white p-2 flex-1 h-36"
                 />
               </div>
             </div>
@@ -115,9 +112,8 @@ const InitiativeForm = ({
               <label htmlFor="name" className="text-marble-white text-base font-bold">
                 Rewards
               </label>
-              <p className="text-concrete text-sm mb-2">Separate rewards by comma</p>
               <Field
-                component="input"
+                component="textarea"
                 name="rewardText"
                 placeholder="e.g. NFT, 1000 USDC"
                 className="mt-1 border border-concrete bg-wet-concrete text-marble-white p-2"
