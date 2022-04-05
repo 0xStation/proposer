384c81f8e31f389a48d3b8510b8fc377a557dcdd
conner <38736612+symmtry@users.noreply.github.com>
2022-04-04 22:40:17 -0700

    Add genBaseUrl for environment-based urls (#261)

---
+++ app/utils/genBaseUrl.ts
@@ -0,0 +1,8 @@
+export const genBaseUrl = () => {
+  return
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : process.env.APP_ENV === "staging"
+    ? "https://staging.station.express"
+    : "https://app.station.express"
+}
