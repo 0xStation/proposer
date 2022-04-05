c672084b0be1f6d43239d82a9b923363bceb9e1f
conner <38736612+symmtry@users.noreply.github.com>
2022-04-04 22:40:04 -0700

    Remove unused enums on AccountInitiative (#264)

---
+++ db/migrations/20220405050538_remove_old_initiative_statuses/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - The values [APPLIED,INVITED,CONTRIBUTOR,INACTIVE] on the enum `AccountInitiativeStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "AccountInitiativeStatus_new" AS ENUM ('INTERESTED', 'CONTRIBUTING', 'PREVIOUSLY_CONTRIBUTED');
+ALTER TABLE "AccountInitiative" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "AccountInitiative" ALTER COLUMN "status" TYPE "AccountInitiativeStatus_new" USING ("status"::text::"AccountInitiativeStatus_new");
+ALTER TYPE "AccountInitiativeStatus" RENAME TO "AccountInitiativeStatus_old";
+ALTER TYPE "AccountInitiativeStatus_new" RENAME TO "AccountInitiativeStatus";
+DROP TYPE "AccountInitiativeStatus_old";
+ALTER TABLE "AccountInitiative" ALTER COLUMN "status" SET DEFAULT 'INTERESTED';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "AccountInitiative" ALTER COLUMN "status" SET DEFAULT E'INTERESTED';
