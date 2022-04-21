96a4028c409ba5e4c10e6aef08ffcb43c9bf176c
Kristen Cheung <krcheung9@gmail.com>
2022-04-21 12:22:18 -0700

    Revert "sign-in with ethereum (#281)"
    This reverts commit 05f388a69cafc560a6652f3840d8c632c6ac328b.

--- app/core/components/ConnectWalletModal.tsx
+++ app/core/components/ConnectWalletModal.tsx
@@ -1,22 +1,15 @@
-import { useCallback } from "react"
 import { Image } from "blitz"
 import Modal from "./Modal"
 import Metamask from "/public/metamask-logo.svg"
 import Coinbase from "/public/coinbase-logo.svg"
 import WalletConnect from "/public/wallet-logo.svg"
 import Banner from "/public/walletconnect-banner.svg"
-import { useConnect, useAccount, useNetwork, useSignMessage } from "wagmi"
+import { useConnect } from "wagmi"
 import { LOCAL_STORAGE } from "../utils/constants"
-import { SiweMessage } from "siwe"
-import useStore from "app/core/hooks/useStore"
 
 const ConnectWalletModal = ({ isWalletOpen, setIsWalletOpen }) => {
   const [{ data: connectData }, connect] = useConnect()
-  const [{ data: accountData }] = useAccount()
-  const [{ data: networkData }] = useNetwork()
-  const [, signMessage] = useSignMessage()
   const [metamaskWallet, walletConnect, coinbaseWallet] = connectData?.connectors
-  const setSiweAddress = useStore((state) => state.setSiweAddress)
 
   // https://github.com/NoahZinsmeister/web3-react/issues/300
   // If a user is connecting their wallet for the first time and has both coinbase and metamask extensions,
@@ -62,57 +55,6 @@ const ConnectWalletModal = ({ isWalletOpen, setIsWalletOpen }) => {
     localStorage.setItem(LOCAL_STORAGE.CONNECTION, "true")
   }
 
-  const signIn = useCallback(async () => {
-    try {
-      const address = accountData?.address
-      const chainId = networkData?.chain?.id
-      if (!address || !chainId) return
-
-      // Fetch random nonce, create SIWE message, and sign with wallet
-      const nonceRes = await fetch("/api/nonce")
-      const message = new SiweMessage({
-        domain: window.location.host,
-        address,
-        statement: "Sign in with Ethereum to the app.",
-        uri: window.location.origin,
-        version: "1",
-        chainId,
-        nonce: await nonceRes.text(),
-      })
-      const signRes = await signMessage({ message: message.prepareMessage() })
-      if (signRes.error) throw signRes.error
-
-      // Verify signature
-      const verifyRes = await fetch("/api/verify", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ message, signature: signRes.data }),
-      })
-      if (!verifyRes.ok) throw new Error("Error verifying message")
-      setSiweAddress(address as string)
-    } catch (error) {
-      // error state
-    }
-  }, [accountData?.address])
-
-  const siweView = (
-    <div>
-      <p className="text-lg text-center mt-4">
-        Authorize your account so that we can remember your preferences.
-      </p>
-      <div className="mt-4 text-center">
-        <button
-          className="border border-marble-white rounded-md content-center hover:bg-wet-concrete px-8 py-1"
-          onClick={signIn}
-        >
-          Remember me
-        </button>
-      </div>
-    </div>
-  )
-
   return (
     <Modal
       title="Enter Station"
@@ -121,74 +63,68 @@ const ConnectWalletModal = ({ isWalletOpen, setIsWalletOpen }) => {
       banner={Banner}
       showTitle={true}
     >
-      {!accountData?.address ? (
-        <>
-          <p className="text-lg text-center mt-4">
-            Connect your wallet to enter Station and explore initiatives.
-          </p>
-          <p className="text-lg text-center">
-            New to web3? Learn how to create a wallet{" "}
-            <a className="text-magic-mint" href="https://www.youtube.com/watch?v=OsRIHlr0_Iw">
-              here
-            </a>
-            .
-          </p>
-          <div className="mt-8">
-            <div className="flex flex-row space-x-3 mx-5 text-marble-white">
-              <button
-                className="flex-1 border border-marble-white rounded-md content-center hover:bg-wet-concrete"
-                onClick={async () => {
-                  // @ts-ignore
-                  await appConnect(metamaskWallet, "metamask")
-                }}
-              >
-                <div className="flex flex-row flex-1 justify-center items-center space-x-2 my-1">
-                  <div className="flex-3/5">
-                    <span>Metamask</span>
-                  </div>
-                  <div className="flex flex-2/5 justify-center items-center">
-                    <Image src={Metamask} alt="Metamask logo." width={21} height={21} />
-                  </div>
-                </div>
-              </button>
-              <button
-                className="flex-1  border border-marble-white rounded-md content-center hover:bg-wet-concrete"
-                onClick={async () => {
-                  // @ts-ignore
-                  await connect(walletConnect)
-                }}
-              >
-                <div className="flex flex-row flex-1 justify-center align-middle items-center space-x-2 my-1 mx-auto">
-                  <div className="flex-3/5">
-                    <span>Wallet Connect</span>
-                  </div>
-                  <div className="flex flex-2/5 justify-center items-center">
-                    <Image src={WalletConnect} alt="Wallet Connect logo." width={19} height={12} />
-                  </div>
-                </div>
-              </button>
-              <button
-                className="flex-1 border border-marble-white rounded-md content-center hover:bg-wet-concrete"
-                onClick={async () => {
-                  // @ts-ignore
-                  await appConnect(coinbaseWallet, "coinbase")
-                }}
-              >
-                <div className="flex flex-row flex-1 justify-center items-center align-middle space-x-2 my-1">
-                  <div className="flex-3/5">
-                    <span>Coinbase</span>
-                  </div>
-                  <div className="flex flex-2/5 justify-center items-center">
-                    <Image src={Coinbase} alt="Coinbase logo." width={16} height={16} />
-                  </div>
-                </div>
-              </button>
+      <p className="text-lg text-center mt-4">
+        Connect your wallet to enter Station and explore initiatives.
+      </p>
+      <p className="text-lg text-center">
+        New to web3? Learn how to create a wallet{" "}
+        <a className="text-magic-mint" href="https://www.youtube.com/watch?v=OsRIHlr0_Iw">
+          here
+        </a>
+        .
+      </p>
+      <div className="mt-8">
+        <div className="flex flex-row space-x-3 mx-5 text-marble-white">
+          <button
+            className="flex-1 border border-marble-white rounded-md content-center hover:bg-wet-concrete"
+            onClick={async () => {
+              // @ts-ignore
+              await appConnect(metamaskWallet, "metamask")
+            }}
+          >
+            <div className="flex flex-row flex-1 justify-center items-center space-x-2 my-1">
+              <div className="flex-3/5">
+                <span>Metamask</span>
+              </div>
+              <div className="flex flex-2/5 justify-center items-center">
+                <Image src={Metamask} alt="Metamask logo." width={21} height={21} />
+              </div>
             </div>
-          </div>
-        </>
-      ) : (
-        siweView
-      )}
+          </button>
+          <button
+            className="flex-1  border border-marble-white rounded-md content-center hover:bg-wet-concrete"
+            onClick={async () => {
+              // @ts-ignore
+              await connect(walletConnect)
+            }}
+          >
+            <div className="flex flex-row flex-1 justify-center align-middle items-center space-x-2 my-1 mx-auto">
+              <div className="flex-3/5">
+                <span>Wallet Connect</span>
+              </div>
+              <div className="flex flex-2/5 justify-center items-center">
+                <Image src={WalletConnect} alt="Wallet Connect logo." width={19} height={12} />
+              </div>
+            </div>
+          </button>
+          <button
+            className="flex-1 border border-marble-white rounded-md content-center hover:bg-wet-concrete"
+            onClick={async () => {
+              // @ts-ignore
+              await appConnect(coinbaseWallet, "coinbase")
+            }}
+          >
+            <div className="flex flex-row flex-1 justify-center items-center align-middle space-x-2 my-1">
+              <div className="flex-3/5">
+                <span>Coinbase</span>
+              </div>
+              <div className="flex flex-2/5 justify-center items-center">
+                <Image src={Coinbase} alt="Coinbase logo." width={16} height={16} />
+              </div>
+            </div>
+          </button>
+        </div>
+      </div>
     </Modal>
   )
 }
