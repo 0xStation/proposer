972b04481b4a516a37db706b1289bbb9ee51e6f7
Michael Gingras <mcg79@cornell.edu>
2022-05-19 23:16:47 -0600

    adding comments to sync-roles

--- app/api/discord/sync-roles.ts
+++ app/api/discord/sync-roles.ts
@@ -1,8 +1,20 @@
 import db from "db"
 import { Terminal } from "app/terminal/types"
 
+/**
+ * API endpoint for "refreshing" roles between discord and station.
+ * When a terminal first connects to station, the discord roles and members with those roles are synced.
+ * However, it is possible for a discord to get more members, or for roles on discord to change (members lose role etc)
+ * So, this function keeps discord and station in sync.
+ *
+ * @param req - terminalId
+ * @param res - 200 or 401
+ * @returns {response: "success"}
+ */
 export default async function handler(req, res) {
   const params = JSON.parse(req.body)
+
+  // Find the terminal we are interested in syncing
   const terminal = (await db.terminal.findUnique({
     where: { id: params.terminalId },
     include: { tags: true },
@@ -13,6 +25,9 @@ export default async function handler(req, res) {
     return
   }
 
+  // get the current members from the guild
+  // TODO: this is going to only accept the first 1000, honestly
+  // we need more tech time to figure out how to fetch > 1000 nicely
   const response = await fetch(
     `${process.env.BLITZ_PUBLIC_API_ENDPOINT}/guilds/${terminal.data.guildId}/members?limit=1000`,
     {
@@ -33,12 +48,18 @@ export default async function handler(req, res) {
     return
   }
 
+  // active tags are the tags in the terminal that are "active" and should be considered for use
   const activeTags = terminal.tags.filter((tag) => tag.active)
+  // filter to get the discordIds, which we need to compare to the tags from the discord guild member response
   const activeCreatedTagDiscordIds = activeTags.map((tag) => tag.discordId || "")
+  // guild members who have one or more of the active tags
   const activeGuildMembers = guildMembers.filter((gm) => {
     return gm.roles.some((r) => activeCreatedTagDiscordIds.includes(r))
   })
 
+  // now that we have the list, we need to shape the guild member object a bit
+  // activeGuildMembers just filtered by which members HAD an active tag
+  // now we need to map to actually get the tags that overlapped
   const filteredActiveGuildMembers = activeGuildMembers.map((gm) => {
     const tagOverlap = activeCreatedTagDiscordIds.filter((tag) => gm.roles.includes(tag))
 
@@ -61,6 +82,8 @@ export default async function handler(req, res) {
   })
 
   filteredActiveGuildMembers.forEach(async (user) => {
+    // creates an account for the active guild member if one does not exist
+    // if one does exist, an empty update map will just skip over this and return the account
     const account = await db.account.upsert({
       where: { discordId: user.discordId },
       update: {},
@@ -75,7 +98,9 @@ export default async function handler(req, res) {
       },
     })
 
-    // case where you remove a tag from an existing member
+    // In order to get the difference between an existing station members tags and their new tags, we need to fetch
+    // their existing tags. This lives on their ticket to the given terminal. Note, this is not a concern for newly
+    // created accounts, since there will be no "difference" of roles - the incoming ones are correct.
     const existingTicket = await db.accountTerminal.findUnique({
       where: {
         accountId_terminalId: {
@@ -88,7 +113,9 @@ export default async function handler(req, res) {
       },
     })
 
-    // new members will not yet have a ticket, don't have to worry about this
+    // building up the tag object
+    // easier to do it this way since only existing tickets may have the "deleteMany" key.
+    // not very "functional" but oh well...
     const tagObject = {}
 
     if (existingTicket) {
@@ -141,5 +168,6 @@ export default async function handler(req, res) {
     })
   })
 
+  // better error handling and success messages prob but im exhausted rn
   res.status(200).json({ response: "success" })
 }
