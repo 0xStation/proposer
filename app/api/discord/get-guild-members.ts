97854181aba2ef139a23a6827c7a087dae263838
Michael Gingras <mcg79@cornell.edu>
2022-05-27 11:48:23 -0600

    changing sync roles to use paginated query

--- app/api/discord/get-guild-members.ts
+++ app/api/discord/get-guild-members.ts
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     })
     const newResults = await response.json()
 
-    if (newResults.code) {
+    if (newResults.code !== undefined) {
       throw newResults.message
     }
 
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
     res.status(200).json({ guildMembers })
     return
   } catch (err) {
-    console.log(err)
+    console.error(err)
     res.status(401).json({ error: err })
   }
 }
--- app/api/discord/sync-roles.ts
+++ app/api/discord/sync-roles.ts
@@ -25,26 +25,44 @@ export default async function handler(req, res) {
     return
   }
 
-  // get the current members from the guild
-  // TODO: this is going to only accept the first 1000, honestly
-  // we need more tech time to figure out how to fetch > 1000 nicely
-  const response = await fetch(
-    `${process.env.BLITZ_PUBLIC_API_ENDPOINT}/guilds/${terminal.data.guildId}/members?limit=1000`,
-    {
+  const paginatedFetch = async (after, previousResults) => {
+    const limit = 1000
+    const baseURL = `${process.env.BLITZ_PUBLIC_API_ENDPOINT}/guilds/${terminal.data.guildId}/members?limit=${limit}`
+    const url = after ? baseURL + `&after=${after}` : baseURL
+    const response = await fetch(url, {
       method: "GET",
       headers: {
         Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
         "Content-Type": "application/json",
       },
+    })
+    const newResults = await response.json()
+
+    if (newResults.code !== undefined) {
+      throw newResults.message
     }
-  )
 
-  const guildMembers = await response.json()
+    const results = [...previousResults, ...newResults]
 
-  if (guildMembers.code) {
-    // discord status codes
-    // https://discord.com/developers/docs/topics/opcodes-and-status-codes
-    res.status(401).json({ error: "Guild not authenticated." })
+    // limit is 1000
+    // 1. there are more results to fetch
+    // 2. we fetched the last 1000, but no way to know
+    // so we need to make another request to check
+    // to request the next page, get the id of the last user in current page to specify as "after"
+    if (newResults.length === 1000) {
+      return paginatedFetch(newResults[999].user.id, results)
+    }
+
+    // anything less than 1000 means this was final page, return results
+    return results
+  }
+
+  let guildMembers
+  try {
+    guildMembers = await paginatedFetch(null, [])
+  } catch (err) {
+    console.error(err)
+    res.status(401).json({ error: err })
     return
   }
 
