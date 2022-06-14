b75f71f699a2b59d69416fe01533cd1a2fb4a03e
Michael Gingras <mcg79@cornell.edu>
2022-06-14 09:44:00 -0400

    blasting away markdown to slate util

--- app/utils/markdownToSlate.ts
+++
@@ -1,9 +0,0 @@
-import markdown from "remark-parse"
-import { unified } from "unified"
-import { remarkToSlate } from "remark-slate-transformer"
-import { Descendant } from "slate"
-
-export const markdownToSlate = (markdownString: string) => {
-  const postprocessor = unified().use(markdown).use(remarkToSlate)
-  return postprocessor.processSync(markdownString).result as Descendant[]
-}
