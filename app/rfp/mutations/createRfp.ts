1d6da10222f407a242f8530ae6ca19abdbe75ef4
Michael Gingras <mcg79@cornell.edu>
2022-06-15 22:00:36 -0400

    Markdown Editor + RFP creation page (#355)

---
+++ app/rfp/mutations/createRfp.ts
@@ -0,0 +1,32 @@
+import db from "db"
+import * as z from "zod"
+
+const CreateRfp = z.object({
+  terminalId: z.number(),
+  fundingAddress: z.string(),
+  authorAddress: z.string(),
+  contentTitle: z.string(),
+  contentBody: z.string(),
+  startDate: z.date(),
+  endDate: z.date().optional(),
+})
+
+export default async function createRfp(input: z.infer<typeof CreateRfp>) {
+  const rfp = await db.rfp.create({
+    data: {
+      fundingAddress: input.fundingAddress,
+      authorAddress: input.authorAddress,
+      terminalId: input.terminalId,
+      startDate: input.startDate,
+      ...(input.endDate && { endDate: input.endDate }),
+      data: {
+        content: {
+          title: input.contentTitle,
+          body: input.contentBody,
+        },
+      },
+    },
+  })
+
+  return rfp
+}
