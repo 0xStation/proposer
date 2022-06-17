2eed16fd6387d9996037f40b906ebe8e83d9f627
conner <38736612+symmtry@users.noreply.github.com>
2022-06-17 10:00:21 -0700

    Add approve check signature generation POC (#360)
    * Check in
    
    * Hide dev tools on non-localhost and add state management
    
    * Add documentation and address feedback
    
    * Add documentation

--- app/types/index.ts
+++ app/types/index.ts
@@ -29,3 +29,17 @@ export type FundingSignature = Signature & {
   // might add the actual details too (recipient, token, amount),
   // but for now FundingSignatures are nested within objects that already store this data
 }
+
+export type TypedDataSignatureDomain = {
+  name: string
+  version: string
+  chainId: number
+  verifyingContract: string
+}
+
+export type TypedDataField = {
+  name: string
+  type: string // solidity type e.g. "address", "uint256", etc.
+}
+
+export type TypedDataTypeDefinition = Record<string, TypedDataField[]>
