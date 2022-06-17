2f2326a5dbbe9bd2e71a618d4e8bb187f1e781d0
Michael Gingras <mcg79@cornell.edu>
2022-06-17 13:40:36 -0400

    Teenage mutation ninja turtles (mutations for RFPs) (#361)
    Co-authored-by: conner <38736612+symmtry@users.noreply.github.com>

---
+++ app/rfp/mutations/reopenRfp.ts
@@ -0,0 +1,28 @@
+import db from "db"
+import * as z from "zod"
+
+// Change the end date to some later date to "reopen" it.
+// note: need to send notification
+const ReopenRfp = z.object({
+  rfpId: z.string(),
+  endDate: z.date(),
+})
+
+export default async function reopenRfp(input: z.infer<typeof ReopenRfp>) {
+  if (input.endDate < new Date()) {
+    throw Error("End date cannot be in the past.")
+  }
+
+  try {
+    const rfp = await db.rfp.update({
+      where: { id: input.rfpId },
+      data: {
+        endDate: input.endDate,
+      },
+    })
+
+    return rfp
+  } catch (error) {
+    throw error
+  }
+}
