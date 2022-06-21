1197717b5d150fd3f48c7291e68096ac6ed9bd7a
Conner Swenberg <cls364@cornell.edu>
2022-06-21 19:11:48 -0400

    Clean up useAllowedNetwork

--- app/contracts/contracts.ts
+++ app/contracts/contracts.ts
@@ -3,13 +3,9 @@ import { CONTRACTS } from "app/core/utils/constants"
 import checkbookFactoryAbi from "./abi/CheckbookFactory.json"
 
 export const useCreateCheckbook = (chainId: number) => {
-  if (!!chainId && chainId != 4 && chainId != 1337) {
-    console.error("Invalid chain selected. Please select Rinkeby or Localhost")
-  }
-  const address = CONTRACTS[chainId]?.CHECKBOOK_FACTORY
   const { writeAsync: createCheckbook } = useContractWrite(
     {
-      addressOrName: address,
+      addressOrName: CONTRACTS[chainId]?.CHECKBOOK_FACTORY,
       contractInterface: checkbookFactoryAbi,
     },
     "create(uint256,address[])" // have to specify arguments because two functions with name create but different arguments
--- app/core/hooks/useAllowedNetwork.ts
+++ app/core/hooks/useAllowedNetwork.ts
@@ -3,22 +3,20 @@ import useStore from "app/core/hooks/useStore"
 
 const useAllowedNetwork = () => {
   const setToastState = useStore((state) => state.setToastState)
-
   const { activeChain } = useNetwork()
 
-  console.log("useAllowedNetwork", activeChain?.id)
-
   if (!!activeChain?.id && activeChain?.id != 4 && activeChain?.id != 1337) {
-    console.error("Invalid chain selected. Please select Rinkeby or Localhost")
+    const error = "Invalid chain selected. Please select Rinkeby or Localhost"
+    console.error(error)
     setToastState({
       isToastShowing: true,
       type: "error",
-      message: "Invalid network, switch to Rinkeby or Localhost.",
+      message: error,
     })
 
     return {
       chainId: activeChain?.id as number,
-      error: "Invalid network, switch to Rinkeby or Localhost",
+      error,
     }
   }
 
