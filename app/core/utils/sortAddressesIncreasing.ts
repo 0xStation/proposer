b54d3a6bbd8c1a0889e501ea3a80086727c6c4e1
conner <38736612+symmtry@users.noreply.github.com>
2022-06-23 23:40:20 -0700

    Checkbook creation from factory (#366)
    * Add checkbook contract connection and create checkbook on form submission
    
    * Update wagmi client to support localhost network
    
    * Add loading spinner on checkbook creation button
    
    * Add success modal for checkbook address copying
    
    * Add dynamic chains to checkbooks and green->purple
    
    * Add comments on checkbook address parsing
    
    * Add rinkeby contract addresses
    
    * Add address sorting for checkbook signers
    
    * Add useAllowedNetwork hook with error toasts
    
    * Clean up useAllowedNetwork
    
    * Revert wagmi client changes and add copy address button
    
    * Update factory address with payable Checkbook

---
+++ app/core/utils/sortAddressesIncreasing.ts
@@ -0,0 +1,10 @@
+import { BigNumber } from "ethers"
+
+// used to sort address lists before sending them in a smart contract transaction
+// address ordering is often used so that contracts can validate no duplicate items
+export const sortAddressesIncreasing = (addresses: string[]): string[] => {
+  return addresses
+    .map((a) => BigNumber.from(a)) // convert addresses to BigNumbers
+    .sort((a, b) => (a.gt(b) ? 1 : -1)) // sort addresses in increasing order
+    .map((n) => n.toHexString()) // convert BigNumbers back to addresses
+}
