05d69bfc888a33c4038f2ae68865182bf7c35cbb
conner <38736612+symmtry@users.noreply.github.com>
2022-06-27 11:42:10 -0700

    Add Checkbook funds accounting (#371)
    * Add useCheckbookFunds hook and token dropdown
    
    * Dyamically populate token options from networks and tags
    
    * Add Check and CheckApproval models
    
    * Add check generation in approval dev tool
    
    * Add check creation db transaction
    
    * Add check pending + cashed aggregation query
    
    * Add pending check by approval quorum query
    
    * Clean up generate check dev tool
    
    * Clean up files
    
    * Add decimal edge cases and BigNumber parsing
    
    * Clean up check generator dev tool
    
    * Add proposal relation and gen migration
    
    * Rename generate check page
    
    * Add token fetching on backend when creating check
    
    * Last cleanup

---
+++ app/core/hooks/useCheckbookFunds.ts
@@ -0,0 +1,42 @@
+import { useBalance } from "wagmi"
+import { BigNumber, utils } from "ethers"
+import { useQuery } from "blitz"
+import getAggregatedCheckAmounts from "app/check/queries/getAggregatedCheckAmounts"
+import { zeroAddress } from "../utils/constants"
+import decimalToBigNumber from "../utils/decimalToBigNumber"
+
+const useCheckbookFunds = (
+  chainId: number,
+  checkbookAddress: string,
+  quorum: number,
+  tokenAddress?: string
+) => {
+  const [aggregatedCheckTotals] = useQuery(
+    getAggregatedCheckAmounts,
+    { checkbookAddress, quorum: quorum, tokenAddress: tokenAddress as string },
+    { suspense: false, enabled: !!tokenAddress }
+  )
+
+  const { data } = useBalance({
+    addressOrName: checkbookAddress,
+    chainId,
+    cacheTime: 10_000, // 10 seconds
+    ...(!!tokenAddress && tokenAddress !== zeroAddress && { token: tokenAddress }), // if tokenAddress is zero address, use gas token (e.g. ETH)
+  })
+  const balance = data?.value || BigNumber.from(0)
+  const decimals = data?.decimals || 0
+
+  // db stores check amounts as decimal so need to remultiply by token's number of decimals
+  const pending = decimalToBigNumber(aggregatedCheckTotals?.pending || 0, decimals)
+  const cashed = decimalToBigNumber(aggregatedCheckTotals?.cashed || 0, decimals)
+
+  return {
+    pending,
+    cashed,
+    available: balance.sub(pending),
+    total: balance.add(cashed),
+    decimals: decimals,
+  }
+}
+
+export default useCheckbookFunds
---
+++ app/utils/fetchTokenDecimals.ts
@@ -0,0 +1,25 @@
+import { AlchemyProvider } from "@ethersproject/providers"
+import { Contract } from "@ethersproject/contracts"
+import networks from "./networks.json"
+import { requireEnv } from "./requireEnv"
+
+// forked from snapshot here: https://github.com/snapshot-labs/snapshot.js/blob/master/src/utils.ts#L46-L86
+export async function fetchTokenDecimals(chainId: number, address: string) {
+  // node provider for making calls to smart contracts
+  const provider = new AlchemyProvider(
+    networks[chainId.toString()].network,
+    requireEnv("ALCHEMY_API_KEY")
+  )
+  // abi for a token's decimals
+  const abi = ["function decimals() view returns (uint8)"]
+  // contract object
+  const contract = new Contract(address, abi, provider)
+
+  try {
+    const res = await contract.functions.decimals!()
+    // res has return values as array in-order, decimals is first (and only) value
+    return res[0]
+  } catch (e) {
+    return Promise.reject(e)
+  }
+}
