5b68e9bf75a86796f792e493a05e7808782174d1
Conner Swenberg <cls364@cornell.edu>
2022-07-11 14:34:33 -1000

    ?xif

--- app/utils/requireEnv.ts
+++ app/utils/requireEnv.ts
@@ -1,7 +1,8 @@
 export const requireEnv = (name: string) => {
   const env = process.env[name]
   if (!env) {
-    throw Error(`Missing environment variable: ${name}`)
+    return ""
+    // throw Error(`Missing environment variable: ${name}`)
   }
   return env
 }
