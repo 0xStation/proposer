68cece6fb1617020b861b3dbf88e4f186edbdf47
Kristen Cheung <krcheung9@gmail.com>
2022-07-13 16:25:30 -0700

    rfp editing permissions (#395)

--- app/pages/terminal/[terminalHandle]/bulletin/rfp/[rfpId]/edit.tsx
+++ app/pages/terminal/[terminalHandle]/bulletin/rfp/[rfpId]/edit.tsx
@@ -1,4 +1,4 @@
-import { BlitzPage, useQuery, useParam } from "blitz"
+import { BlitzPage, useQuery, useParam, GetServerSideProps, getSession, invoke } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getCheckbooksByTerminal from "app/checkbook/queries/getCheckbooksByTerminal"
 import getTerminalByHandle from "app/terminal/queries/getTerminalByHandle"
@@ -7,6 +7,46 @@ import getRfpById from "app/rfp/queries/getRfpById"
 import { Terminal } from "app/terminal/types"
 import { Rfp } from "app/rfp/types"
 import { Checkbook } from "app/checkbook/types"
+import hasAdminPermissionsBasedOnTags from "app/permissions/queries/hasAdminPermissionsBasedOnTags"
+
+export const getServerSideProps: GetServerSideProps = async ({ params, req, res }) => {
+  const session = await getSession(req, res)
+
+  if (!session?.userId) {
+    return {
+      redirect: {
+        destination: `/terminal/${params?.terminalHandle}/bulletin/rfp/${params?.rfpId}/info`,
+        permanent: false,
+      },
+    }
+  }
+
+  const terminal = await invoke(getTerminalByHandle, { handle: params?.terminalHandle as string })
+
+  const hasTagAdminPermissions = await invoke(hasAdminPermissionsBasedOnTags, {
+    terminalId: terminal?.id as number,
+    accountId: session?.userId as number,
+  })
+
+  const rfp = await invoke(getRfpById, { id: params?.rfpId })
+
+  if (
+    (!terminal?.data?.permissions?.accountWhitelist?.includes(session?.siwe?.address as string) &&
+      !hasTagAdminPermissions) ||
+    session.userId !== rfp?.author?.id
+  ) {
+    return {
+      redirect: {
+        destination: `/terminal/${params?.terminalHandle}/bulletin/rfp/${params?.rfpId}/info`,
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
 
 const EditRfpPage: BlitzPage = () => {
   const terminalHandle = useParam("terminalHandle") as string
