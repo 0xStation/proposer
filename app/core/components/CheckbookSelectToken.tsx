63a383b8df7d0cf6754c9f2eefcea398b095fbe9
Michael Gingras <mcg79@cornell.edu>
2022-07-13 22:13:19 -0600

    Web 583 only display tokens that are available (#396)
    * new hook for checkbook availability
    
    * adding token availablilty to token select
    
    * a bit of cleanup

--- app/core/components/CheckbookSelectToken.tsx
+++ app/core/components/CheckbookSelectToken.tsx
@@ -1,6 +1,6 @@
-import getFundingTokens from "app/core/utils/getFundingTokens"
 import { Terminal } from "app/terminal/types"
 import { Checkbook } from "app/checkbook/types"
+import useCheckbookAvailability from "app/core/hooks/useCheckbookAvailability"
 
 // A component that shows a dropdown of the tokens in a given checkbook
 const CheckbookSelectToken = ({
@@ -12,7 +12,14 @@ const CheckbookSelectToken = ({
   checkbook: Checkbook
   options?: any
 }) => {
-  const tokenOptions = getFundingTokens(checkbook, terminal)
+  const tokenTotals = useCheckbookAvailability(checkbook, terminal)
+
+  const validTokens =
+    (tokenTotals &&
+      tokenTotals.filter((token) => {
+        return token.available > 0
+      })) ||
+    []
 
   return (
     <div>
@@ -21,7 +28,7 @@ const CheckbookSelectToken = ({
         className={`w-full bg-wet-concrete border border-concrete rounded p-1 mt-3`}
       >
         <option>Select token</option>
-        {tokenOptions.map((token, i) => {
+        {validTokens.map((token, i) => {
           return (
             <option key={i} value={token.address}>
               {token.symbol}
