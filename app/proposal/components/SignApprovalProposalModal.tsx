d75cbc34803dbe811a9933fcde130ecc5dfffa60
Conner Swenberg <cls364@cornell.edu>
2022-07-26 16:39:13 -0700

    Refactor signature generation to library

--- app/proposal/components/SignApprovalProposalModal.tsx
+++ app/proposal/components/SignApprovalProposalModal.tsx
@@ -5,13 +5,12 @@ import useStore from "app/core/hooks/useStore"
 import createCheck from "app/check/mutations/createCheck"
 import approveProposal from "app/proposal/mutations/approveProposal"
 import { ZERO_ADDRESS } from "app/core/utils/constants"
-import decimalToBigNumber from "app/core/utils/decimalToBigNumber"
-import { TypedDataTypeDefinition } from "app/types"
 import getChecksByProposalId from "../../check/queries/getChecksByProposalId"
 import { Check } from "@prisma/client"
 import { truncateString } from "app/core/utils/truncateString"
 import { formatDate } from "app/core/utils/formatDate"
 import { InformationCircleIcon } from "@heroicons/react/solid"
+import { genCheckSignatureMessage } from "app/signatures/check"
 
 export const SignApprovalProposalModal = ({ isOpen, setIsOpen, proposal, rfp, checks }) => {
   const router = useRouter()
@@ -99,69 +98,9 @@ export const SignApprovalProposalModal = ({ isOpen, setIsOpen, proposal, rfp, ch
   }
 
   const approveCheck = async (check: Check, decimals: number) => {
-    // 3 parts to a typed-data signature:
-    //   1. domain: metadata about the contract and where it is located
-    //   2. types: definition of the typed object being signed at once
-    //   3. value: values that fit into the types definition
-
-    /**
-     * DOMAIN
-     * top-level definition of where this signature will be used
-     * `name` is hardcoded to "Checkbook" in each contract
-     * `version` is also harcoded to "1" in each contract
-     * `chainId` should be the actual id for the contract, 4 is hardcoded for Rinkeby testing
-     * `verifyingContract` is the address of the contract that will be consuming this signature (the checkbook)
-     */
-    const domain = {
-      name: "Checkbook", // keep hardcoded
-      version: "1", // keep hardcoded
-      chainId: check.chainId,
-      verifyingContract: check.fundingAddress,
-    }
-
-    /**
-     * TYPES
-     * definition of the object representing a single check, order and types matter
-     * types per value are using Solidity types
-     * `Check` is capitalized to mimic Solidity structs
-     * `recipient` is the address of the check recipient
-     * `token` is the address of the currency token where the zero address represents ETH
-     * `amount` is the uint representation of amount of tokens to move (requires BigNumber package in js/ts)
-     * `deadline` is the expiry date of this check represented as a unix timestamp
-     * `nonce` is the unique identifier for this check to prevent double spending
-     */
-    const types: TypedDataTypeDefinition = {
-      Check: [
-        { name: "recipient", type: "address" },
-        { name: "token", type: "address" },
-        { name: "amount", type: "uint256" },
-        { name: "deadline", type: "uint256" },
-        { name: "nonce", type: "uint256" },
-      ],
-    }
-
-    /**
-     * VALUE
-     * instantiation of a typed object reflecting `types`
-     * all required data comes from the `Check` entity to guarantee consistent values across all of its approvals
-     * notice that `decimalToBigNumber` is needed to convert db values into contract-readable values
-     */
-
-    const value = {
-      recipient: check.recipientAddress,
-      token: check.tokenAddress,
-      amount: decimalToBigNumber(check.tokenAmount, decimals),
-      deadline: check.deadline.valueOf(),
-      nonce: check.nonce,
-    }
-
     // prompt the Metamask signature modal
     try {
-      const signature = await signApproval({
-        domain,
-        types,
-        value,
-      })
+      const signature = await signApproval(genCheckSignatureMessage(check, decimals))
       return signature
     } catch (e) {
       setToastState({
--- app/types/index.ts
+++ app/types/index.ts
@@ -16,30 +16,3 @@ export enum Role {
   WEEKEND_COMMUTER = "WEEKEND COMMUTER",
   VISITOR = "VISITOR",
 }
-
-export type Signature = {
-  address: string
-  message: string
-  signature: string
-  timestamp: Date
-}
-
-export type FundingSignature = Signature & {
-  approved: boolean
-  // might add the actual details too (recipient, token, amount),
-  // but for now FundingSignatures are nested within objects that already store this data
-}
-
-export type TypedDataSignatureDomain = {
-  name: string
-  version: string
-  chainId: number
-  verifyingContract: string
-}
-
-export type TypedDataField = {
-  name: string
-  type: string // solidity type e.g. "address", "uint256", etc.
-}
-
-export type TypedDataTypeDefinition = Record<string, TypedDataField[]>
