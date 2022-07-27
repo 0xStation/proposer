3056dc2f7e918567aaf335041a6037f344499680
Conner Swenberg <cls364@cornell.edu>
2022-07-27 13:05:23 -0700

    Add logic for ETH vs. ERC20 transfers in check generation

--- app/signatures/check.ts
+++ app/signatures/check.ts
@@ -1,4 +1,5 @@
 import { ZERO_ADDRESS } from "app/core/utils/constants"
+import { Interface } from "@ethersproject/abi"
 
 export const genCheckSignatureMessage = (
   chainId,
@@ -9,6 +10,25 @@ export const genCheckSignatureMessage = (
   token: string,
   amount
 ) => {
+  let target
+  let value
+  let data
+
+  if (token === ZERO_ADDRESS) {
+    // if transferring ETH, call recipient directly with value, no call data
+    target = recipient
+    value = amount
+    data = "0x"
+  } else {
+    // if transferring ERC20, call contract with no value, providing encoded data for `transfer` function
+    target = token
+    value = 0
+    const erc20TransferInterface = new Interface([
+      "function transfer(address to, uint256 amount) external returns (bool)",
+    ])
+    data = erc20TransferInterface.encodeFunctionData("transfer", [recipient, amount])
+  }
+
   return {
     domain: {
       name: "Checkbook", // keep hardcoded
@@ -30,12 +50,12 @@ export const genCheckSignatureMessage = (
     value: {
       txns: [
         {
-          nonce: nonce,
+          nonce,
           deadline: deadline.valueOf(),
           executor: ZERO_ADDRESS,
-          target: recipient,
-          value: amount,
-          data: "0x",
+          target,
+          value,
+          data,
         },
       ],
     },
