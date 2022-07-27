5a668522aa6d16d9f5dd5d631e4888f6a2427c8f
conner <38736612+symmtry@users.noreply.github.com>
2022-07-27 13:48:29 -0700

    Change check signature scheme to generic transaction (#438)
    * wip
    
    * Update abi and contracts with fixed versions
    
    * Add logic for ETH vs. ERC20 transfers in check generation
    
    * Add documentation on claimIndex argument
    
    * Update checkbook deployment

--- app/check/components/CashCheckModal.tsx
+++ app/check/components/CashCheckModal.tsx
@@ -35,14 +35,9 @@ export const CashCheckModal = ({
       const parsedCheck = checkToContractTypes(check)
 
       const transaction = await cashCheck({
-        args: [
-          parsedCheck.recipient,
-          parsedCheck.token,
-          parsedCheck.amount,
-          parsedCheck.deadline,
-          parsedCheck.nonce,
-          parsedCheck.signatures,
-        ],
+        // first argument is which transaction in list to execute
+        // before we implement split-checks, there will only be one txn so we hardcode the index to `0`
+        args: [0, parsedCheck.txns, parsedCheck.signatures],
       })
 
       await cashCheckMutation({
