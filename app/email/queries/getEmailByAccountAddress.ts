dd59b9fcbbccce71774cf92dcf88d3cc01ea3e09
Kristen Cheung <krcheung9@gmail.com>
2022-07-26 22:56:45 -0700

    email verification (#427)
    * initial verify email
    
    * create new verification code
    
    * email verification modal
    
    * resend verification email
    
    * smol fixes
    
    * /verify -> /verify/email
    
    * add existing email to get verified modal if user hasn't verified yet
    
    * address pr comments
    
    * remove comment
    
    * address nits

---
+++ app/email/queries/getEmailByAccountAddress.ts
@@ -0,0 +1,24 @@
+import { getEmail } from "app/utils/privy"
+import * as z from "zod"
+
+const GetEmailByAccountAddress = z.object({
+  address: z.string(),
+})
+
+export default async function getEmailByAccountAddress(
+  input: z.infer<typeof GetEmailByAccountAddress>
+) {
+  try {
+    const params = GetEmailByAccountAddress.parse(input)
+    const email = await getEmail(params.address as string)
+
+    if (!email) {
+      return null
+    }
+
+    return email
+  } catch (err) {
+    console.error("Error fetching user's email. Failed with err: ", err)
+    return null
+  }
+}
