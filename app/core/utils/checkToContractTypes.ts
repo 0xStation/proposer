ef6206db0c4d573d2f7052680af250d6403fe86b
conner <38736612+symmtry@users.noreply.github.com>
2022-07-27 13:48:29 -0700

    Change check signature scheme to generic transaction (#438)
    * wip
    
    * Update abi and contracts with fixed versions
    
    * Add logic for ETH vs. ERC20 transfers in check generation
    
    * Add documentation on claimIndex argument
    
    * Update checkbook deployment

--- app/core/utils/checkToContractTypes.ts
+++ app/core/utils/checkToContractTypes.ts
@@ -1,17 +1,11 @@
 import { Check } from "app/check/types"
 import { CheckApprovalMetadata } from "app/checkApproval/types"
-import decimalToBigNumber from "./decimalToBigNumber"
-import { ZERO_ADDRESS } from "./constants"
 import { BigNumber } from "ethers"
 
 // prepare check object from database to types ready for ethers for signatures or transactions
 export const checkToContractTypes = (check: Check) => {
   return {
-    recipient: check.recipientAddress,
-    token: check.tokenAddress,
-    amount: decimalToBigNumber(check.tokenAmount, 18),
-    deadline: check.deadline.valueOf(),
-    nonce: check.nonce,
+    txns: check.data.signatureMessage.value.txns,
     // sort signatures in order of increasing signer address
     signatures: check.approvals
       .sort((a, b) =>
