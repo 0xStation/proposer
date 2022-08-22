ed552fbe74e4f57a6dc262c2c70ce751d8770b89
Conner Swenberg <cls364@cornell.edu>
2022-08-22 12:04:57 -0700

    Fix getProposalById query status

--- app/proposal/queries/getProposalById.ts
+++ app/proposal/queries/getProposalById.ts
@@ -1,7 +1,7 @@
 import db from "db"
 import * as z from "zod"
 import { Proposal, ProposalStatus as ProductProposalStatus } from "../types"
-import { computeProposalStatus } from "../utils"
+import { ProposalStatus as PrismaProposalStatus } from "db"
 
 const GetProposalById = z.object({
   id: z.string(),
@@ -36,19 +36,11 @@ export default async function getProposalById(params: z.infer<typeof GetProposal
     return null
   }
 
-  const firstCheck = proposal.checks[0]
-  if (firstCheck) {
-    // todo - need to remove quorum from checkbook if we ever want to remove dep of checkbook
-    const quorum = firstCheck.checkbook.quorum
-    return {
-      ...proposal,
-      status: computeProposalStatus(proposal.approvals.length, quorum),
-    } as unknown as Proposal
-    // if first check does not exist, it means there can be no approvals. It must be in the sumbitted state
-  } else {
-    return {
-      ...proposal,
-      status: ProductProposalStatus.SUBMITTED,
-    } as unknown as Proposal
-  }
+  return {
+    ...proposal,
+    status:
+      proposal.status === PrismaProposalStatus.PUBLISHED
+        ? ProductProposalStatus.SUBMITTED
+        : proposal.status,
+  } as unknown as Proposal
 }
--- db/migrations/20220820012751_add_proposal_statuses_approved_in_review/data.ts
+++ db/migrations/20220820012751_add_proposal_statuses_approved_in_review/data.ts
@@ -1,7 +1,7 @@
 import db from "../../index"
 import { ProposalStatus as PrismaProposalStatus } from "@prisma/client"
 
-// run with: blitz db seed -f db/data-migrations/1-proposal-status.ts
+// run with: blitz db seed -f db/migrations/20220820012751_add_proposal_statuses_approved_in_review/data.ts
 const seed = async () => {
   // Update proposal approval status based on signature count and quorum
 
