afb927c1c7e2a70c395d6573acb6c94f03cbe8c9
Michael Gingras <mcg79@cornell.edu>
2022-09-08 13:48:33 -0600

    adding modal to storybook component library

---
+++ app/core/components/sds/overlays/Modal.tsx
@@ -0,0 +1,117 @@
+import { Dialog, Transition } from "@headlessui/react"
+import { Fragment } from "react"
+import Exit from "/public/exit-button.svg"
+
+interface ModalProps {
+  /**
+   * If the modal is open
+   */
+  open: boolean
+  /**
+   * callback to run that triggers the modal open/close
+   */
+  toggle: (open) => void
+  /**
+   * Heading title of the modal
+   */
+  title?: string
+  /**
+   * If the title should we shown
+   */
+  showTitle?: boolean
+  /**
+   * If there is a subtitle of the modal
+   */
+  subtitle?: string
+  /**
+   * Modal contents
+   */
+  children?: any
+  /**
+   * tbd...
+   */
+  error?: boolean
+  /**
+   * Width of the modal, as a tailwind css class (max-w-2xl for example)
+   */
+  width?: string
+}
+const Modal = ({
+  title,
+  open,
+  toggle,
+  subtitle,
+  children,
+  error,
+  showTitle = true,
+  width = "max-w-2xl",
+}: ModalProps) => {
+  return (
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={() => toggle(!open)}>
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-tunnel-black bg-opacity-50" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span className="inline-block h-screen align-middle" aria-hidden="true">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div
+              className={`inline-block w-full ${width} my-8 overflow-hidden rounded text-left align-middle transition-all transform bg-tunnel-black border ${
+                error ? "border-torch-red" : "border-concrete"
+              }`}
+            >
+              {showTitle && (
+                <div className="w-full h-full relative">
+                  <button
+                    className="text-marble-white absolute z-50 right-2 top-2"
+                    onClick={() => toggle(!open)}
+                  >
+                    <img src="/exit-button.svg" alt="Close button" />
+                  </button>
+                </div>
+              )}
+              <div className="px-4 pb-4">
+                {showTitle && (
+                  <Dialog.Title
+                    as="h3"
+                    className="text-3xl font-medium leading-8 text-marble-white text-center"
+                  >
+                    {title}
+                  </Dialog.Title>
+                )}
+                {subtitle && (
+                  <Dialog.Description className="text-lg font-medium text-marble-white text-center mt-4">
+                    {subtitle}
+                  </Dialog.Description>
+                )}
+                {children}
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
+
+export default Modal
