e6c311810a2bb2ae1a58dc4ccd89522b1b6ea39e
Michael Gingras <mcg79@cornell.edu>
2022-09-12 22:06:19 -0600

    nossr

--- app/core/components/Listbox.tsx
+++ app/core/components/Listbox.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useState, useEffect } from "react"
 import { Listbox, Transition } from "@headlessui/react"
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/solid"
+import NoSsr from "app/core/components/NoSsr"
 
 type ListboxItem = {
   id: number
@@ -40,61 +41,70 @@ const CustomListbox = ({ items, onChange, defaultValue, error }: ListboxComponen
     }
   }, [defaultValue])
 
+  const classNames = error
+    ? "border-torch-red bg-torch-red text-tunnel-black"
+    : "border-marble-white text-marble-white"
+  console.log(classNames)
+
   return (
-    <Listbox value={selected} onChange={onChangeAndSetSelected}>
-      <div
-        className={`relative w-[175px] rounded-md border ${
-          error
-            ? "border-torch-red bg-torch-red text-tunnel-black"
-            : "border-marble-white text-marble-white"
-        }`}
-      >
-        <Listbox.Button className="relative w-full cursor-default h-[35px] pl-3 pr-10 text-left shadow-md focus:outline-none sm:text-sm">
-          <span className="block truncate font-bold text-base">
-            {error ? error.message : selected.name}
-          </span>
-          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-            <ChevronDownIcon
-              className={`h-5 w-5 ${error ? "text-tunnel-black" : "text-marble-white"}`}
-              aria-hidden="true"
-            />
-          </span>
-        </Listbox.Button>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+    <NoSsr>
+      <Listbox value={selected} onChange={onChangeAndSetSelected}>
+        <div
+          className={`relative w-[175px] rounded-md border ${
+            error
+              ? "border-torch-red bg-torch-red text-tunnel-black"
+              : "border-marble-white text-marble-white"
+          }`}
         >
-          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-wet-concrete py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50 text-marble-white">
-            {items.map((item, itemIdx) => (
-              <Listbox.Option
-                key={itemIdx}
-                className={({ active, selected }) =>
-                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                    active || selected ? "bg-electric-violet" : "text-gray-900"
-                  }`
-                }
-                value={item}
-              >
-                {({ selected }) => (
-                  <>
-                    <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
-                      {item.name}
-                    </span>
-                    {selected ? (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-marble-white">
-                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+          <Listbox.Button className="relative w-full cursor-default h-[35px] pl-3 pr-10 text-left shadow-md focus:outline-none sm:text-sm">
+            <span className="block truncate font-bold text-base">
+              {error ? error.message : selected.name}
+            </span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronDownIcon
+                className={`h-5 w-5 ${error ? "text-tunnel-black" : "text-marble-white"}`}
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-wet-concrete py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50 text-marble-white">
+              {items.map((item, itemIdx) => (
+                <Listbox.Option
+                  key={itemIdx}
+                  className={({ active, selected }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active || selected ? "bg-electric-violet" : "text-gray-900"
+                    }`
+                  }
+                  value={item}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${selected ? "font-medium" : "font-normal"}`}
+                      >
+                        {item.name}
                       </span>
-                    ) : null}
-                  </>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Transition>
-      </div>
-    </Listbox>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-marble-white">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </NoSsr>
   )
 }
 
