55803c9a5550f454b4ed478b2a5d5c0fab06faff
Kristen Cheung <krcheung9@gmail.com>
2022-09-16 10:38:47 -0700

    skeleton buttons (#566)

--- app/core/components/CopyBtn.tsx
+++ app/core/components/CopyBtn.tsx
@@ -1,13 +1,17 @@
-import { Routes } from "blitz"
 import { useState } from "react"
 import { LinkIcon } from "@heroicons/react/solid"
-import { genPathFromUrlObject } from "app/utils"
 
-export const CopyBtn = ({ textToWrite = "" }) => {
+export const CopyBtn = ({
+  textToWrite = "",
+  className,
+}: {
+  textToWrite?: string
+  className?: string
+}) => {
   const [isTextCopied, setIsTextCopied] = useState<boolean>(false)
   return (
     // TODO: add icons to buttons for sds, currently can't use unemphasized button
-    <div className="group inline">
+    <div className={`${className} group inline`}>
       <button
         onClick={() => {
           navigator.clipboard.writeText(textToWrite)
