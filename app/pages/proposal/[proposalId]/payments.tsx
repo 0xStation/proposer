6a162c0de74b4f248c4d5e2ab5b738211800cf3f
kristencheung <krcheung9@gmail.com>
2022-09-16 02:52:13 -0700

    add attach payment back

--- app/pages/proposal/[proposalId]/payments.tsx
+++ app/pages/proposal/[proposalId]/payments.tsx
@@ -1,27 +1,112 @@
-import { useParam, useQuery } from "blitz"
+import { useParam, useQuery, BlitzPage } from "blitz"
+import { useEffect, useState } from "react"
 import { ProposalViewHeaderNavigation } from "app/proposalNew/components/viewPage/ProposalViewHeaderNavigation"
-import { BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getProposalNewById from "app/proposalNew/queries/getProposalNewById"
+import { ProposalRoleType } from "@prisma/client"
+import { addressesAreEqual } from "app/core/utils/addressesAreEqual"
+import useStore from "app/core/hooks/useStore"
+import { areApprovalsComplete } from "app/proposalNew/utils"
+import getProposalNewApprovalsByProposalId from "app/proposalNewApproval/queries/getProposalNewApprovalsByProposal"
+import truncateString from "app/core/utils/truncateString"
+import Button, { ButtonType } from "app/core/components/sds/buttons/Button"
+import ExecutePaymentModal from "app/proposalNew/components/ExecutePaymentModal"
+import { CheckCircleIcon } from "@heroicons/react/solid"
 
 export const ProposalPayments: BlitzPage = () => {
+  const activeUser = useStore((state) => state.activeUser)
   const proposalId = useParam("proposalId") as string
+  const [approvalsComplete, setApprovalsComplete] = useState<boolean>(false)
+  const [isExecutePaymentModalOpen, setIsExecutePaymentModalOpen] = useState<boolean>(false)
   const [proposal] = useQuery(
     getProposalNewById,
     { id: proposalId },
     { suspense: false, refetchOnWindowFocus: false, refetchOnReconnect: false }
   )
+  const [approvals] = useQuery(
+    getProposalNewApprovalsByProposalId,
+    { proposalId: proposal?.id as string },
+    { suspense: false, refetchOnWindowFocus: false, refetchOnReconnect: false }
+  )
+
+  useEffect(() => {
+    if (proposal?.roles && approvals && areApprovalsComplete(proposal?.roles, approvals)) {
+      setApprovalsComplete(true)
+    }
+  }, [proposal?.roles, approvals])
+
+  const proposalContainsPayment = (proposal?.payments && proposal?.payments.length > 0) || false
+  const userIsPayer = proposal?.roles.some(
+    (role) =>
+      role.role === ProposalRoleType.CLIENT &&
+      addressesAreEqual(activeUser?.address || "", role.address)
+  )
+  const paymentComplete = !!proposal?.payments?.[0]?.transactionHash
+
+  const showPayInformation = proposalContainsPayment && approvalsComplete && userIsPayer
+  // && !paymentComplete
+
   return (
     <Layout title="View Payments">
+      <ExecutePaymentModal
+        isOpen={isExecutePaymentModalOpen}
+        setIsOpen={setIsExecutePaymentModalOpen}
+        payment={proposal?.payments?.[0]}
+      />
       <div className="w-full md:min-w-1/2 md:max-w-2xl mx-auto h-full">
         <ProposalViewHeaderNavigation />
-        <div className="w-full h-full flex items-center flex-col sm:mt-0">
-          <p className="text-2xl font-bold w-[295px] text-center mt-44">No payments queued</p>
-          <p className="text-base w-[320px] text-center">
-            Payments queue and history will be displayed here after the proposal has been signed by
-            all collaborators.
-          </p>
-        </div>
+        {showPayInformation ? (
+          <div className={` mt-9 border border-b border-concrete rounded-2xl px-6 py-9`}>
+            <div className=" text-concrete uppercase text-xs font-bold w-full flex flex-row items-end">
+              <span className="basis-32 mb-2 tracking-wider">From</span>
+              <span className="basis-32 ml-6 mb-2 tracking-wider">To</span>
+              <span className="basis-28 ml-6 mb-2 tracking-wider">Token</span>
+              <span className="basis-28 ml-6 mb-2 tracking-wider">Amount</span>
+            </div>
+            {proposal?.payments?.map((payments) => (
+              <div className="w-full flex flex-row items-end" key={payments?.id}>
+                <span className="basis-32 mb-2 tracking-wider">
+                  {truncateString(payments?.senderAddress)}
+                </span>
+                <span className="basis-32 ml-6 mb-2 tracking-wider">
+                  {truncateString(payments?.recipientAddress)}
+                </span>
+                <span className="basis-28 ml-6 mb-2 tracking-wider">
+                  {payments?.data?.token?.symbol}
+                </span>
+                <span className="basis-28 ml-6 mb-2 tracking-wider">{payments?.amount}</span>
+              </div>
+            ))}
+            {!paymentComplete ? (
+              <Button
+                overrideWidthClassName="w-full"
+                className="mt-4"
+                type={ButtonType.Secondary}
+                onClick={() => setIsExecutePaymentModalOpen(true)}
+              >
+                Pay
+              </Button>
+            ) : (
+              <button
+                className="mt-4 mb-2 sm:mb-0 border rounded w-[300px] sm:w-[400px] md:w-[614px] h-[35px] mr-3 opacity-70 bg-electric-violet border-electric-violet text-tunnel-black cursor-not-allowed"
+                disabled
+              >
+                <CheckCircleIcon className="h-5 w-5 inline mb-1 mr-2" />
+                Paid
+              </button>
+            )}
+          </div>
+        ) : Boolean(proposal?.roles) && Boolean(approvals) ? (
+          <div className="w-full h-full flex items-center flex-col sm:mt-0">
+            <h1 className="text-2xl font-bold w-[295px] text-center mt-44">No payments queued</h1>
+            <p className="text-base w-[320px] text-center mt-2.5">
+              Payments queue and history will be displayed here after the proposal has been signed
+              by all collaborators.
+            </p>
+          </div>
+        ) : (
+          <div className="mt-9 h-[260px] w-full flex flex-row rounded-2xl bg-wet-concrete shadow border-solid motion-safe:animate-pulse" />
+        )}
       </div>
     </Layout>
   )
