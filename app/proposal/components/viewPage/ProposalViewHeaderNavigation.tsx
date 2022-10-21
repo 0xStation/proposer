9c7e7936128f8597e18a98972cf611b3575b8aed
Michael Gingras <mcg79@cornell.edu>
2022-10-21 13:25:24 -0600

    Update app/proposal/components/viewPage/ProposalViewHeaderNavigation.tsx
    Co-authored-by: conner <38736612+symmtry@users.noreply.github.com>

--- app/proposal/components/viewPage/ProposalViewHeaderNavigation.tsx
+++ app/proposal/components/viewPage/ProposalViewHeaderNavigation.tsx
@@ -96,11 +96,10 @@ export const ProposalViewHeaderNavigation = () => {
         )
       : ""
 
-  const proposalContainsPayment = (proposal?.payments && proposal?.payments.length > 0) || false
+  const = currentMilestone = proposal?.milestones.find(milestone => milestone.index === proposal.currentMilestoneIndex)
+  const currentMilestoneContainsPendingPayment = (proposal?.payments?.filter(payment => !payment.transactionHash && payment.milestoneId === currentMilestone?.id).length > 0) || false
 
-  const showPayInformation =
-    proposalContainsPayment &&
-    (proposal?.status === ProposalStatus.APPROVED || proposal?.status === ProposalStatus.COMPLETE)
+  const showPayInformation = !!currentMilestone && currentMilestoneContainsPendingPayment
 
   return (
     <>
