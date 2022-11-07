3d55d8c9e81fc120e7eedab9726f974d31975482
Conner Swenberg <cls364@cornell.edu>
2022-11-07 00:58:08 -0500

    Refactor seed script to update all rfps

--- db/seed/addRfpSocialConnection.ts
+++ db/seed/addRfpSocialConnection.ts
@@ -2,13 +2,9 @@ import { SocialConnection } from "app/rfp/types"
 import db from "db"
 
 const seed = async () => {
-  const templateId = "835ef848-91c1-46da-bdf9-4b0a277fe808"
+  const foxesTemplateId = "835ef848-91c1-46da-bdf9-4b0a277fe808"
 
-  const rfps = await db.rfp.findMany({
-    where: {
-      templateId,
-    },
-  })
+  const rfps = await db.rfp.findMany({})
 
   rfps.forEach(async (rfp) => {
     await db.rfp.update({
@@ -18,7 +14,8 @@ const seed = async () => {
       data: {
         data: {
           ...Object(rfp.data),
-          requiredSocialConnections: [SocialConnection.DISCORD],
+          requiredSocialConnections:
+            rfp.templateId === foxesTemplateId ? [SocialConnection.DISCORD] : [],
         },
       },
     })
