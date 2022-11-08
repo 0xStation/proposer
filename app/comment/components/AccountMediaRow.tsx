9fe863fb478df1b2d5b6b1e45f331d53b0931714
Michael Gingras <mcg79@cornell.edu>
2022-11-08 12:04:17 -0700

    fix

--- app/comment/components/AccountMediaRow.tsx
+++ app/comment/components/AccountMediaRow.tsx
@@ -3,15 +3,15 @@ import useDisplayAddress from "app/core/hooks/useDisplayAddress"
 import { Sizes } from "app/core/utils/constants"
 
 const AccountMediaRow = ({ account }) => {
-  const displayName = useDisplayAddress(account.address)
+  const displayAddress = useDisplayAddress(account.address)
 
   return (
     <div className="flex flex-row space-x-2 items-start">
-      <Avatar size={Sizes.SM} address={account.address as string} pfpUrl={account.pfpUrl} />
+      <Avatar size={Sizes.SM} address={account.address as string} pfpUrl={account?.data?.pfpUrl} />
       {account.data?.name && (
         <span className="text-marble-white text-base">{account.data?.name}</span>
       )}
-      <span className="text-concrete text-sm">{displayName.text}</span>
+      <span className="text-concrete text-sm">{displayAddress.text}</span>
     </div>
   )
 }
