963202bd622974f811e1fa7350adf15a6fbef1cf
Michael Gingras <mcg79@cornell.edu>
2022-11-08 14:28:14 -0700

    updating auth for comment mutation

--- app/comment/mutations/createComment.ts
+++ app/comment/mutations/createComment.ts
@@ -1,6 +1,10 @@
 import db from "db"
+import { invoke } from "@blitzjs/rpc"
 import * as z from "zod"
 import { Ctx } from "blitz"
+import getRolesByProposalId from "app/proposalRole/queries/getRolesByProposalId"
+import { addressesAreEqual } from "app/core/utils/addressesAreEqual"
+import { AddressType } from "@prisma/client"
 
 const CreateComment = z.object({
   commentBody: z.string(),
@@ -11,19 +15,32 @@ const CreateComment = z.object({
 export default async function createComment(input: z.infer<typeof CreateComment>, ctx: Ctx) {
   const params = CreateComment.parse(input)
 
-  const roles = await db.proposalRole.findMany({
-    where: {
-      proposalId: params.proposalId,
-    },
-  })
-
-  const addressesWithValidRoles = roles.map((role) => role.address)
-  ctx.session.$authorize(addressesWithValidRoles, [])
+  // const roles = await db.proposalRole.findMany({
+  //   where: {
+  //     proposalId: params.proposalId,
+  //   },
+  // })
 
   if (!ctx.session.$publicData.siwe) {
     console.error("cannot comment without a user session.")
-    return null
+    throw new Error("cannot comment without a user session.")
   }
+
+  const roles = await invoke(getRolesByProposalId, { proposalId: params.proposalId as string })
+  const addressesWithValidRoles =
+    roles
+      ?.map((role) => {
+        if (role?.account?.addressType === AddressType.WALLET) {
+          return role.account.address
+        } else if (role?.account?.addressType === AddressType.SAFE) {
+          return role?.account?.data?.signers || []
+        }
+        return []
+      })
+      .flat()
+      .filter((e) => e !== undefined) || []
+
+  ctx.session.$authorize(addressesWithValidRoles, [])
 
   await db.comment.create({
     data: {
