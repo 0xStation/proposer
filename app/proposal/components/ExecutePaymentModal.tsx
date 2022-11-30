62b45691996e6b1c9cef024dfc7492eba80ef1d7
Michael Gingras <mcg79@cornell.edu>
2022-11-30 11:10:56 -0700

    dropping attach tx code

--- app/proposal/components/ExecutePaymentModal.tsx
+++ app/proposal/components/ExecutePaymentModal.tsx
@@ -1,65 +1,21 @@
 import Modal from "app/core/components/Modal"
 import { useState } from "react"
-import { AttachTransactionForm } from "app/proposalPayment/components/AttachTransactionForm"
 import { DirectPayment } from "app/proposalPayment/components/DirectPayment"
 
-enum Tab {
-  DIRECT_PAYMENT = "DIRECT_PAYMENT",
-  ATTACH_TRANSACTION = "ATTACH_TRANSACTION",
-}
 export const ExecutePaymentModal = ({ isOpen, setIsOpen, proposal, milestone, payment }) => {
-  const [selectedTab, setSelectedTab] = useState<Tab>(Tab.DIRECT_PAYMENT)
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   return (
     <Modal open={isOpen} toggle={setIsOpen}>
       <div className="p-2">
-        <div className="space-x-4 text-l mt-4">
-          <span
-            className={`${
-              selectedTab === Tab.DIRECT_PAYMENT && "border-b mb-[-1px] font-bold"
-            } cursor-pointer`}
-            onClick={() => {
-              console.log(isOpen)
-              setSelectedTab(Tab.DIRECT_PAYMENT)
-              console.log(isOpen)
-            }}
-          >
-            Direct payment
-          </span>
-          <span
-            className={`${
-              selectedTab === Tab.ATTACH_TRANSACTION && "border-b mb-[-1px] font-bold"
-            } cursor-pointer`}
-            onClick={() => setSelectedTab(Tab.ATTACH_TRANSACTION)}
-          >
-            Attach transaction
-          </span>
-        </div>
-        {selectedTab === Tab.DIRECT_PAYMENT ? (
-          <>
-            <DirectPayment
-              proposal={proposal}
-              milestone={milestone}
-              payment={payment}
-              isLoading={isLoading}
-              setIsLoading={setIsLoading}
-              setIsOpen={setIsOpen}
-              setTabAttachTransaction={() => {
-                setSelectedTab(Tab.ATTACH_TRANSACTION)
-              }}
-            />
-          </>
-        ) : (
-          <AttachTransactionForm
-            milestone={milestone}
-            payment={payment}
-            chainId={payment?.data?.token.chainId}
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-            setIsOpen={setIsOpen}
-          />
-        )}
+        <DirectPayment
+          proposal={proposal}
+          milestone={milestone}
+          payment={payment}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+          setIsOpen={setIsOpen}
+        />
       </div>
     </Modal>
   )
