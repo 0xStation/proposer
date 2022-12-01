706f9b9478f6f33c23f1ad767cb7c96b7f8dc99e
Michael Gingras <mcg79@cornell.edu>
2022-11-30 17:28:56 -0700

    stashing

--- pages/api/webhook/parse-gnosis-tx.ts
+++ pages/api/webhook/parse-gnosis-tx.ts
@@ -2,10 +2,24 @@ import db from "db"
 import { api } from "app/blitz-server"
 import { NextApiRequest, NextApiResponse } from "next"
 import SaveTransactionHashToPayments from "app/proposal/mutations/saveTransactionToPayments"
-import { GNOSIS_EXECUTION_SUCCESS_EVENT_HASH } from "app/core/utils/constants"
+import {
+  GNOSIS_CHANGED_THRESHOLD_EVENT_HASH,
+  GNOSIS_EXECUTION_SUCCESS_EVENT_HASH,
+} from "app/core/utils/constants"
 import { multicall } from "app/utils/rpcMulticall"
 import { Account } from "app/account/types"
 import { ProposalPayment, ProposalPaymentStatus } from "app/proposalPayment/types"
+
+const fetchGnosisThreshold = async (chainId: string, targetAddress: string) => {
+  const abi = ["function getThreshold() public view returns (uint256)"]
+
+  const metadata = await multicall(chainId, abi, [
+    { targetAddress: targetAddress, functionSignature: "getThreshold", callParameters: [] },
+  ])
+
+  const threshold = metadata[0][0]
+  return threshold.toNumber()
+}
 
 const fetchGnosisNonce = async (chainId: string, targetAddress: string) => {
   const abi = ["function nonce() view returns (uint256 nonce)"]
@@ -18,38 +32,18 @@ const fetchGnosisNonce = async (chainId: string, targetAddress: string) => {
   return nonce
 }
 
-export default api(async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = req.body
-  const streamId = response.streamId
-  console.log(response)
-
-  // eventually look for error logs as well
-  const gnosisTxSuccessEventLog = response.logs.find(
-    (log) => log.topic0 === GNOSIS_EXECUTION_SUCCESS_EVENT_HASH
-  )
-
-  // I experimented with returning 400s but moralis did not like receiving a 400 in response to a rebhook
-  // so instead we just terminate early
-  if (!gnosisTxSuccessEventLog) {
-    return res.end()
-  }
-
-  const account = (await db.account.findFirst({
+const handleChangedThreshold = async (account) => {
+  const threshold = await fetchGnosisThreshold(String(account.data.chainId || 1), account.address)
+  const accountProposals = await db.proposal.findMany({
     where: {
-      moralisStreamId: streamId,
+      // participant
     },
-  })) as Account
+  })
+}
 
-  // honestly, something has probably gone wrong in the database if we recieve a webhook
-  // for an account that does not exist or is not a safe.
-  if (!account || account.addressType !== "SAFE") {
-    return res.end()
-  }
-
+const handleExecutionSuccess = async (account, log) => {
   const nonce = await fetchGnosisNonce(String(account.data.chainId || 1), account.address)
 
-  // TODO: get feedback on this line -- really annoying that we have to parse through every single payment
-  // ever from a particular account just to find the matching payment
   const accountPayments = (await db.proposalPayment.findMany({
     where: {
       senderAddress: account.address,
@@ -77,10 +71,10 @@ export default api(async function handler(req: NextApiRequest, res: NextApiRespo
     !payment ||
     mostRecentPaymentAttempt.status !== ProposalPaymentStatus.QUEUED
   ) {
-    return res.end()
+    throw new Error("Could not find payment attempt")
   }
 
-  const txData = gnosisTxSuccessEventLog.data
+  const txData = log.data
   const safeTxHash = txData.slice(0, 66)
 
   // webhook is a successful transaction response
@@ -90,7 +84,7 @@ export default api(async function handler(req: NextApiRequest, res: NextApiRespo
         await SaveTransactionHashToPayments({
           milestoneId: payment.milestoneId,
           proposalId: payment.proposalId,
-          transactionHash: gnosisTxSuccessEventLog.transactionHash,
+          transactionHash: log.transactionHash,
           paymentId: payment.id,
         })
       } catch (e) {
@@ -109,7 +103,7 @@ export default api(async function handler(req: NextApiRequest, res: NextApiRespo
             ...payment.data.history.slice(0, payment.data.history.length - 1),
             {
               ...mostRecentPaymentAttempt,
-              transactionHash: gnosisTxSuccessEventLog.transactionHash,
+              transactionHash: log.transactionHash,
               status: ProposalPaymentStatus.REJECTED,
               timestamp: new Date(),
             },
@@ -118,7 +112,62 @@ export default api(async function handler(req: NextApiRequest, res: NextApiRespo
       },
     })
   }
+}
 
-  res.statusCode = 200
+const handleExecutionFailure = async () => {
+  // pass
+}
+
+export default api(async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const t = await fetchGnosisThreshold("5", "0xd0e09D3D8C82A8B92e3B1284C5652Da2ED9aEc31")
+  const response = req.body
+  const streamId = response.streamId
+
+  console.log(response)
+
+  const account = (await db.account.findFirst({
+    where: {
+      moralisStreamId: streamId,
+    },
+  })) as Account
+
+  // honestly, something has probably gone wrong in the database if we recieve a webhook
+  // for an account that does not exist or is not a safe.
+  if (!account || account.addressType !== "SAFE") {
+    return res.end()
+  }
+
+  const gnosisChangedThresholdEventLog = response.events.find(
+    (log) => log.topic0 === GNOSIS_CHANGED_THRESHOLD_EVENT_HASH
+  )
+
+  if (gnosisChangedThresholdEventLog) {
+    await handleChangedThreshold(account)
+    res.statusCode = 200
+    res.end()
+  }
+
+  // eventually look for error logs as well
+  const gnosisTxSuccessEventLog = response.logs.find(
+    (log) => log.topic0 === GNOSIS_EXECUTION_SUCCESS_EVENT_HASH
+  )
+
+  if (!gnosisTxSuccessEventLog) {
+    res.statusCode = 500
+    return res.end()
+  }
+
+  try {
+    await handleExecutionSuccess(account, gnosisTxSuccessEventLog)
+    res.statusCode = 200
+    res.end()
+  } catch (e) {
+    console.log(e)
+    res.statusCode = 500
+    res.end()
+  }
+
+  // default response - 500
+  res.statusCode = 500
   res.end()
 })
