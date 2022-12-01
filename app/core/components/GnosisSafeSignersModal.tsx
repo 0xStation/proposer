873c1875234ff8a264833d2a08cbd233dc014f53
Conner Swenberg <cls364@cornell.edu>
2022-12-01 14:46:31 -0500

    Add ENS name to gnosis signer modal

--- app/core/components/GnosisSafeSignersModal.tsx
+++ app/core/components/GnosisSafeSignersModal.tsx
@@ -1,6 +1,26 @@
 import Modal from "./Modal"
-import truncateString from "app/core/utils/truncateString"
 import { formatDate } from "app/core/utils/formatDate"
+import useDisplayAddress from "../hooks/useDisplayAddress"
+
+const SignerRow = ({ signerAddress, hasSigned, signedAt }) => {
+  const { text: signerDisplayAddress } = useDisplayAddress(signerAddress)
+
+  return (
+    <>
+      <p className="col-span-2 text-sm self-center">{signerDisplayAddress}</p>
+      {hasSigned ? (
+        <span className="text-sm bg-magic-mint font-bold text-tunnel-black inline-block rounded-full px-2 py-1 w-fit">
+          Signed
+        </span>
+      ) : (
+        <span className="text-sm bg-neon-carrot font-bold text-tunnel-black inline-block rounded-full px-2 py-1 w-fit">
+          Pending
+        </span>
+      )}
+      <span className="text-sm self-center">{signedAt}</span>
+    </>
+  )
+}
 
 const GnosisSafeSignersModal = ({ isOpen, setIsOpen, role }) => {
   return (
@@ -8,32 +28,22 @@ const GnosisSafeSignersModal = ({ isOpen, setIsOpen, role }) => {
       <div className="p-6">
         <h2 className="text-2xl font-bold">Signers List</h2>
         <h3 className="mt-2">This is a list of all signers for this Safe.</h3>
-        <div className="grid grid-cols-3 mt-4 gap-y-4">
-          <span className="text-xs text-light-concrete uppercase font-bold">Signer</span>
+        <div className="grid grid-cols-4 mt-4 gap-y-4">
+          <span className="col-span-2 text-xs text-light-concrete uppercase font-bold">Signer</span>
           <span className="text-xs text-light-concrete uppercase font-bold">Status</span>
           <span className="text-xs text-light-concrete uppercase font-bold">Date</span>
           {role.account.data.signers.map((signer, idx) => {
+            const hasSigned = role.signatures.some((signature) => signature.address === signer)
+            const signedAt = formatDate(
+              role.signatures.filter((signature) => signature.address === signer)[0]?.timestamp
+            )
             return (
-              <>
-                <p key={`signer-${idx}`} className="text-sm self-center">
-                  {truncateString(signer)}
-                </p>
-                {role.signatures.some((signature) => signature.address === signer) ? (
-                  <span className="text-sm bg-magic-mint font-bold text-tunnel-black inline-block rounded-full px-2 py-1 w-fit">
-                    Signed
-                  </span>
-                ) : (
-                  <span className="text-sm bg-neon-carrot font-bold text-tunnel-black inline-block rounded-full px-2 py-1 w-fit">
-                    Pending
-                  </span>
-                )}
-                <span className="text-sm self-center">
-                  {formatDate(
-                    role.signatures.filter((signature) => signature.address === signer)[0]
-                      ?.timestamp
-                  )}
-                </span>
-              </>
+              <SignerRow
+                signerAddress={signer}
+                hasSigned={hasSigned}
+                signedAt={signedAt}
+                key={`signer-${idx}`}
+              />
             )
           })}
         </div>
