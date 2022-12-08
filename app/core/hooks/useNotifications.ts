3d21bb35a358e8345c05774461215268db4a26ee
Michael Gingras <mcg79@cornell.edu>
2022-12-08 13:10:33 -0700

    adding more notification types

--- app/core/hooks/useNotifications.ts
+++ app/core/hooks/useNotifications.ts
@@ -1,14 +1,68 @@
 import { getAntiCSRFToken } from "@blitzjs/auth"
 
+export interface NotificationFromUserType {
+  address: string
+}
+
+export interface NotificationType {
+  type: string
+  subscriberId: string
+  payload: {
+    from: NotificationFromUserType | string
+    title: string
+    note: string
+    extra?: string
+  }
+}
+
 export interface NewCommentNotificationType {
   recipient: string
   payload: {
-    comment_body: string
+    from: NotificationFromUserType | "STATION"
+    proposalTitle: string
+    commentBody: string
+  }
+}
+
+export interface NewProposalNotificationType {
+  recipient: string
+  payload: {
+    from: NotificationFromUserType | "STATION"
+    proposalTitle: string
+  }
+}
+
+export interface PaymentNotificationType {
+  recipient: string
+  payload: {
+    from: NotificationFromUserType | "STATION"
+    proposalTitle: string
+    paymentAmount: number
+    paymentToken: string
+  }
+}
+
+export interface NewRFPSubmissionNotificationType {
+  recipient: string
+  payload: {
+    from: NotificationFromUserType | "STATION"
+    proposalTitle: string
+    rfpTitle: string
+    commentBody: string
+  }
+}
+
+export interface ProposalApprovalNotificationType {
+  recipient: string
+  payload: {
+    from: NotificationFromUserType | "STATION"
+    proposalTitle: string
+    commentBody: string
   }
 }
 
 export const useNotifications = () => {
-  const fetcher = async (formData: any) => {
+  const fetcher = async (formData: NotificationType) => {
     await fetch("/api/novu/send-notification", {
       method: "POST",
       headers: {
@@ -25,12 +79,87 @@ export const useNotifications = () => {
     const data = {
       type,
       subscriberId: formData.recipient,
-      payload: formData.payload,
+      payload: {
+        from: formData.payload.from,
+        title: "Commented on your proposal",
+        note: formData.payload.proposalTitle,
+        extra: formData.payload.commentBody,
+      },
     }
+
+    fetcher(data)
+  }
+
+  const sendNewProposalNotification = async (formData: NewProposalNotificationType) => {
+    const type = "new-proposal"
+
+    const data = {
+      type,
+      subscriberId: formData.recipient,
+      payload: {
+        title: "Sent you a proposal",
+        from: formData.payload.from,
+        note: formData.payload.proposalTitle,
+      },
+    }
+
+    fetcher(data)
+  }
+
+  const sendPaymentNotification = async (formData: PaymentNotificationType) => {
+    const type = "paid"
+
+    const data = {
+      type,
+      subscriberId: formData.recipient,
+      payload: {
+        title: `Paid you ${formData.payload.paymentAmount} ${formData.payload.paymentToken}`,
+        from: formData.payload.from,
+        note: formData.payload.proposalTitle,
+      },
+    }
+
+    fetcher(data)
+  }
+
+  const sendNewRFPSubmissionNotification = async (formData: NewRFPSubmissionNotificationType) => {
+    const type = "new-rfp-submission"
+
+    const data = {
+      type,
+      subscriberId: formData.recipient,
+      payload: {
+        from: formData.payload.from,
+        title: "Submitted a proposal to your RFP",
+        note: `${formData.payload.rfpTitle} - ${formData.payload.proposalTitle}`,
+      },
+    }
+
+    fetcher(data)
+  }
+
+  const sendProposalApprovalNotification = async (formData: NewProposalNotificationType) => {
+    // maybe make these enums
+    const type = "proposal-approved"
+
+    const data = {
+      type,
+      subscriberId: formData.recipient,
+      payload: {
+        from: "STATION",
+        title: `${formData.payload.proposalTitle} has been approved!`,
+        note: formData.payload.proposalTitle,
+      },
+    }
+
     fetcher(data)
   }
 
   return {
     sendNewCommentNotification,
+    sendNewProposalNotification,
+    sendPaymentNotification,
+    sendNewRFPSubmissionNotification,
+    sendProposalApprovalNotification,
   }
 }
--- pages/noti/index.tsx
+++ pages/noti/index.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react"
 import { BlitzPage } from "@blitzjs/next"
+import Image from "next/image"
 import { useNotifications } from "app/core/hooks/useNotifications"
 import { NovuProvider, useNotifications as useNovuNotifications } from "@novu/notification-center"
+import StationLogo from "public/station-letters.svg"
+import { convertJSDateToDateAndTime } from "app/core/utils/convertJSDateToDateAndTime"
+import { notificationTemplateIdToContentMap } from "app/core/utils/constants"
 
 function CustomNotificationCenter() {
   const { notifications, fetchNextPage, hasNextPage, fetching, markAsSeen, refetch } =
@@ -12,10 +16,9 @@ function CustomNotificationCenter() {
   }, [])
 
   console.log(notifications)
-  console.log(fetching)
 
   return (
-    <>
+    <div className="max-w-screen-xl mx-auto mt-20">
       {/* Table */}
       <table className="w-full table-auto">
         {/* Columns */}
@@ -23,7 +26,7 @@ function CustomNotificationCenter() {
           <tr className="border-b border-concrete">
             <th className="text-xs uppercase text-light-concrete pb-2 pl-4 text-left">User</th>
             <th className="text-xs uppercase text-light-concrete pb-2 text-left">Notification</th>
-            <th className="text-xs uppercase text-light-concrete pb-2 text-left">Time</th>
+            <th className="text-xs uppercase text-light-concrete pb-2 text">Time</th>
           </tr>
         </thead>
         <tbody>
@@ -33,15 +36,27 @@ function CustomNotificationCenter() {
                 className="border-b border-wet-concrete cursor-pointer hover:bg-wet-concrete"
                 key={`row-${idx}`}
               >
-                <td>{notification.content}</td>
-                <td>{notification.content}</td>
-                <td>{notification.createdAt}</td>
+                <td className="py-4 pl-4 align-top">
+                  <Image src={StationLogo} alt="Station logo" height={16} width={50} />
+                </td>
+                <td className="py-4 space-y-2">
+                  <span className="block">{notification.payload.title}</span>
+                  <span className="block text-marble-white text-opacity-80 text-sm">
+                    {notification.payload.note}
+                  </span>
+                  <span className="block text-marble-white">{notification.payload.extra}</span>
+                </td>
+                <td className="py-4 align-top text-right pr-4">
+                  <span className="text-sm">
+                    {convertJSDateToDateAndTime({ timestamp: new Date(notification.createdAt) })}
+                  </span>
+                </td>
               </tr>
             )
           })}
         </tbody>
       </table>
-    </>
+    </div>
   )
 }
 
@@ -54,14 +69,18 @@ const Demo: BlitzPage = () => {
         onClick={async () => {
           await sendNewCommentNotification({
             recipient: "123abcdefg",
-            payload: { comment_body: "testing" },
+            payload: {
+              from: "STATION",
+              proposalTitle: "Proposal title",
+              commentBody: "this is the comment_body",
+            },
           })
         }}
         className="px-2 py-2 bg-neon-carrot text-tunnel-black"
       >
         TRIGGER NOTIFICATION
       </button>
-      <NovuProvider subscriberId={"123abc"} applicationIdentifier={"mbe4KpHO7Mj5"}>
+      <NovuProvider subscriberId={"123abcdefg"} applicationIdentifier={"mbe4KpHO7Mj5"}>
         <CustomNotificationCenter />
       </NovuProvider>
     </>
