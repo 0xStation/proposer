001ec7b3c048eecb42724ad4c3558fe7d08007b2
Kristen Cheung <krcheung9@gmail.com>
2022-12-19 12:21:55 -0800

    Foxes fix: make sure rfp status is reading from source of truth (#809)
    * rfp status was looking at two different sources to maintain its state, change it to rely on one
    
    * fix foxes page numbers

--- app/rfp/components/RfpCard.tsx
+++ app/rfp/components/RfpCard.tsx
@@ -1,8 +1,7 @@
 import { RfpStatus } from "@prisma/client"
-import { getTotalPaymentAmount, getPaymentToken } from "app/template/utils"
 import { RouteUrlObject } from "blitz"
 import Link from "next/link"
-import React, { RefObject, useEffect, useState } from "react"
+import React, { RefObject } from "react"
 import { Rfp } from "../types"
 import RfpSchedule from "./metadata/RfpSchedule"
 import LookingForPill from "./LookingForPill"
@@ -13,20 +12,35 @@ import { Account } from "app/account/types"
 import { useScheduleCallback } from "app/core/hooks/useScheduleCallback"
 
 export const RfpCard = React.forwardRef(
-  ({ account, rfp, href }: { account: Account; rfp: Rfp; href: RouteUrlObject }, ref) => {
-    // cards are populated from a bulk query which is more expensive to invalidate
-    // so we give local state to cards to visually update
-    const [rfpStatus, setRfpStatus] = useState<RfpStatus>(rfp?.status)
-    useScheduleCallback({ callback: () => setRfpStatus(RfpStatus.OPEN), date: rfp?.startDate })
-    useScheduleCallback({ callback: () => setRfpStatus(RfpStatus.CLOSED), date: rfp?.endDate })
+  (
+    {
+      account,
+      rfp,
+      href,
+      refetchRfp,
+    }: { account: Account; rfp: Rfp; href: RouteUrlObject; refetchRfp?: () => void },
+    ref
+  ) => {
+    useScheduleCallback({
+      callback: () => {
+        if (refetchRfp) refetchRfp()
+      },
+      date: rfp?.startDate,
+    })
+    useScheduleCallback({
+      callback: () => {
+        if (refetchRfp) refetchRfp()
+      },
+      date: rfp?.endDate,
+    })
 
     const RfpCardContent = ({ account, rfp }) => (
       <>
         <div>
           {/* STATUS PILLS */}
           <div className="flex flex-row flex-wrap gap-1">
-            <RfpStatusPill status={rfpStatus} />
-            {rfpStatus !== RfpStatus.CLOSED && (
+            <RfpStatusPill status={rfp?.status} />
+            {rfp?.status !== RfpStatus.CLOSED && (
               <LookingForPill role={rfp?.data?.proposal?.proposerRole} />
             )}
           </div>
@@ -35,7 +49,7 @@ export const RfpCard = React.forwardRef(
         </div>
         <div className="mt-6 flex flex-col space-y-4 h-[120px]">
           <RfpReward rfpProposalPayment={rfp?.data?.proposal?.payment} />
-          <RfpSchedule status={rfpStatus} startDate={rfp?.startDate} endDate={rfp?.endDate} />
+          <RfpSchedule status={rfp?.status} startDate={rfp?.startDate} endDate={rfp?.endDate} />
         </div>
         <div className="flex flex-row mt-6 justify-between">
           <span>
